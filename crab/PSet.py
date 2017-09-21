8131cd3783b314877eb90616294d98844b0ac683
Andrea <andrea.rizzi@cern.ch>
2017-09-21 11:33:34 +0200

    add some comments

--- crab/PSet.py
+++ crab/PSet.py
@@ -1,8 +1,11 @@
+#this fake PSET is needed for local test and for crab to figure the output filename
+#you do not need to edit it unless you want to do a local test using a different input file than
+#the one marked below
 import FWCore.ParameterSet.Config as cms
 process = cms.Process('NANO')
 process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
 process.source.fileNames = [
-	'../../NanoAOD/test/save.root'
+	'../../NanoAOD/test/lzma.root' ##you can change only this line
 ]
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
 process.output = cms.OutputModule("PoolOutputModule", fileName = cms.untracked.string('tree.root'))
