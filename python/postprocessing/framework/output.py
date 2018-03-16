377cee4040eda2361a9800142326deb4c5861fb3
Piergiulio <piergiulio.lenzi@cern.ch>
2018-03-16 16:03:15 +0100

    allow replacing an existing branch

--- python/postprocessing/framework/output.py
+++ python/postprocessing/framework/output.py
@@ -12,7 +12,7 @@ class OutputBranch:
         self.buff   = array(_rootBranchType2PythonArray[rootBranchType], n*[0. if rootBranchType in 'FD' else 0])
         self.lenVar = lenVar
         self.n = n
-        #check if a branch was already there. In case SetStatus to 0
+        #check if a branch was already there. In case, scratch it first 
         for b in tree.GetListOfBranches():
           if b.GetName() == name:
              tree.GetListOfBranches().Remove(b)
