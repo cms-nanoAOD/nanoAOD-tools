a91838e205357e4e438ecbbf9ae0973a899f8a0a
Andrea <andrea.rizzi@cern.ch>
2018-03-21 09:10:48 +0100

    add reduce mantissa

--- src/classes.h
+++ src/classes.h
@@ -2,8 +2,10 @@
 #include "PhysicsTools/NanoAODTools/interface/PyJetResolutionScaleFactorWrapper.h"
 #include "PhysicsTools/NanoAODTools/interface/PyJetParametersWrapper.h"
 #include "PhysicsTools/NanoAODTools/interface/WeightCalculatorFromHistogram.h"
+#include "PhysicsTools/NanoAODTools/interface/ReduceMantissa.h"
 
 PyJetResolutionWrapper jetRes;
 PyJetResolutionScaleFactorWrapper jetResScaleFactor;
 PyJetParametersWrapper jetParams;
 WeightCalculatorFromHistogram wcalc;
+ReduceMantissaToNbitsRounding red(12);
