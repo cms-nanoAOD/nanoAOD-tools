d6d7f25b61c654ae1e827ef146bc0e748e671798
Piergiulio <piergiulio.lenzi@cern.ch>
2018-05-03 17:18:02 +0200

    removed printout

--- python/postprocessing/framework/output.py
+++ python/postprocessing/framework/output.py
@@ -14,13 +14,10 @@ class OutputBranch:
         self.n = n
         #check if a branch was already there 
         existingBranch = tree.GetBranch(name)
-        print name, existingBranch
         if (existingBranch):
-          #print name,"is an existing branch"
           self.branch = existingBranch
           self.branch.SetAddress(self.buff)
         else:  
-          #print name,"is a new branch"
           if lenVar != None:
             self.branch = tree.Branch(name, self.buff, "%s[%s]/%s" % (name,lenVar,rootBranchType))
           elif n == 1:
