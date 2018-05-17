fc950bd0d8db7c34e8b3e123a951102514860822
Enrique Palencia Cortezon <enrique.palencia.cortezon@cern.ch>
2018-05-17 18:19:55 +0200

    Updated some parameters

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -1,26 +1,31 @@
 from WMCore.Configuration import Configuration
 config = Configuration()
 
+# https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile
+
 config.section_("General")
-config.General.requestName = 'NanoPost1'
+config.General.requestName = 'NanoLocalDir'
 config.General.transferLogs=True
+
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
 config.JobType.scriptExe = 'crab_script.sh'
 config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py'] #hadd nano will not be needed once nano tools are in cmssw
 config.JobType.sendPythonFolder	 = True
+
 config.section_("Data")
-config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM'
+config.Data.inputDataset = '/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17NanoAOD-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/NANOAODSIM'
 config.Data.inputDBS = 'global'
-#config.Data.splitting = 'FileBased'
-config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 100
-config.Data.totalUnits = 2000
+config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = 3
 #config.Data.inputDBS='phys03'
-config.Data.outLFNDirBase = '/store/user/jrgonzal/'
+config.Data.outLFNDirBase = '/store/user/username/nanoAODcrab'
 config.Data.publication = True
-config.Data.outputDatasetTag = 'NanoTestPost'
+config.Data.outputDatasetTag = 'Test17May2018'
+
 config.section_("Site")
 config.Site.storageSite = "T2_ES_IFCA"
 
