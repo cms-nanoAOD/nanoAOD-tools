94df2e2f04a2d553169c8e2f2762e69d088165d9
siewyanhoh <jeffrey_siewyan@hotmail.com>
2018-11-18 22:15:41 +0100

    added PtZ correction computation and HT computation

--- python/postprocessing/operational/objCleaning.py
+++ python/postprocessing/operational/objCleaning.py
@@ -29,18 +29,27 @@ class objCleaning(Module): # MHT producer, unclean jets only (no lepton overlap 
         pass
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.initReaders(inputTree) # initReaders must be called in beginFile
+        self.FilesName=inputFile
         self.out = wrappedOutputTree
         #CleanObjectCollection
         self.out.branch("cleanedJet" ,"I", 100);
         self.out.branch("cleanedMuon",  "I", 100);
         self.out.branch("cleanedElectron",  "I", 100);
+        self.out.branch("MHTju_pt",  "F");
+        self.out.branch("MHTju_phi", "F");
+        self.out.branch("ZPtCorr","F");
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
 
     def initReaders(self,tree): # this function gets the pointers to Value and ArrayReaders and sets them in the C++ worker class
         self._ttreereaderversion = tree._ttreereaderversion # self._ttreereaderversion must be set AFTER all calls to tree.valueReader or tree.arrayReader
-
+        for branch in tree.GetListOfBranches():
+            if( branch.GetName() == "nGenPart" ):
+                self.Genpart=True
+                print "nGenPart exist, this is MC"
+                break
+            
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
 
@@ -52,7 +61,8 @@ class objCleaning(Module): # MHT producer, unclean jets only (no lepton overlap 
         electrons = Collection(event, "Electron")
         muons = Collection(event, "Muon")
         jets = Collection(event, "Jet")
-        genparts = Collection(event, "GenPart")
+        if self.Genpart:
+            genparts = Collection(event, "GenPart")
         
         # Cleaning Jet
         Jet_Clean=[0]*100
@@ -89,6 +99,17 @@ class objCleaning(Module): # MHT producer, unclean jets only (no lepton overlap 
                 if jet.chHEF>0.1: Jet_Clean[num]=1
                 if jet.neHEF>0.2: Jet_Clean[num]=1
 
+        # HT Computation
+        HTpt=0.
+        HTphi=0.
+        for num, jet in enumerate(jets):
+            if jet.puId<4: continue;
+            if jet.pt<30: continue;
+            if fabs(jet.eta)>2.5: continue;
+            if Jet_Clean[num]==0: continue;
+            HTpt+=jet.pt
+            HTphi+=jet.phi
+
         # Cleaning on lepton
         Muon_Clean=[0]*100
         Muon_DRjet=[0.]*100
@@ -104,15 +125,16 @@ class objCleaning(Module): # MHT producer, unclean jets only (no lepton overlap 
                 if dr < DRMuJet:
                     DRMuJet = dr
             Muon_DRjet[numMu]= DRMuJet #distance between muon-jet
-            
-            # match gen stable and final muon
-            valuepair=closest(lep,genparts, lambda x,y: True if y.status==1 and y.pdgId==x.pdgId else False) #stable genpart; same flavor
-            if valuepair[1]>0.04: continue; #within DR<0.04
-            Muon_Clean[numMu]=1
-            #momID=valuepair[0].genPartIdxMother # mother of gen muon
-            #print "Mother of gen-muon status : ", genparts[momID].status, " and pdgId : ", genparts[momID].pdgId
-            #grandmomID=genparts[momID].genPartIdxMother # grandmother of mother of gen muon
-            #print "Grandmother of gen-muon status : ", genparts[grandmomID].status, " and pdgId : ", genparts[grandmomID].pdgId
+
+            if self.Genpart:
+                # match gen stable and final muon
+                valuepair=closest(lep,genparts, lambda x,y: True if y.status==1 and y.pdgId==x.pdgId else False) #stable genpart; same flavor
+                if valuepair[1]>0.04: continue; #within DR<0.04
+                Muon_Clean[numMu]=1
+                #momID=valuepair[0].genPartIdxMother # mother of gen muon
+                #print "Mother of gen-muon status : ", genparts[momID].status, " and pdgId : ", genparts[momID].pdgId
+                #grandmomID=genparts[momID].genPartIdxMother # grandmother of mother of gen muon
+                #print "Grandmother of gen-muon status : ", genparts[grandmomID].status, " and pdgId : ", genparts[grandmomID].pdgId
 
         for numEl,lep in enumerate(electrons):
             #find the nearest cleanjet
@@ -123,15 +145,36 @@ class objCleaning(Module): # MHT producer, unclean jets only (no lepton overlap 
                     DRElJet = dr
             Electron_DRjet[numEl]= DRElJet #distance between electron-jet
 
-            # match gen stable and final electron
-            valuepair=closest(lep,genparts, lambda x,y: True if y.status==1 and y.pdgId==x.pdgId else False)
-            if valuepair[1]>0.04: continue;
-            Electron_Clean[numEl]=1
-            
+            if self.Genpart:
+                # match gen stable and final electron
+                valuepair=closest(lep,genparts, lambda x,y: True if y.status==1 and y.pdgId==x.pdgId else False)
+                if valuepair[1]>0.04: continue;
+                Electron_Clean[numEl]=1
+
+        # Perform PtZ correction computation
+        Zweight = 1.
+        NgenZ=0
+        if self.Genpart and "DYJetsToLL" in self.FilesName.GetName().split('/')[-1].split('_')[0]:
+            for gen in genparts:
+                if gen.pdgId!=23: continue;
+                if gen.status!=62: continue;
+                NgenZ+=1
+                if NgenZ!=1: continue;
+                Zpt=gen.pt
+                if Zpt<20: Zweight=1.2
+                if Zpt>20 and Zpt<30: Zweight=1.
+                if Zpt>30 and Zpt<40: Zweight=0.75
+                if Zpt>40 and Zpt<50: Zweight=0.65
+                if Zpt>50 and Zpt<200: Zweight=0.65-0.00034*Zpt
+                if Zpt>200: Zweight=0.6
+                
             
         self.out.fillBranch("cleanedJet", Jet_Clean);
         self.out.fillBranch("cleanedMuon", Muon_Clean);
         self.out.fillBranch("cleanedElectron", Electron_Clean);
+        self.out.fillBranch("MHTju_pt", HTpt)
+        self.out.fillBranch("MHTju_phi", HTphi)
+        self.out.fillBranch("ZPtCorr", Zweight);
         return True
         
 # define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
--- test.sh
+++ test.sh
@@ -10,5 +10,5 @@ elif [ "$test1" == "1" ];then
     python scripts/nano_postproc.py -c "nTau==0 && Muon_pt[0]>20" -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.mhtjuProducerCpp mhtju . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
 elif [ "$test1" == "2" ];then
     echo "SKIMMING + SLIMMING + CLEANING"
-    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.objCleaning cleaning . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
+    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.objCleaning cleaning . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/VHv9-skim_reprocess_v1/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1-v2.root
 fi
