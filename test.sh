772ad6affacd47bdbcb67b5c25ecbbf3eb17dbd2
siewyanhoh <jeffrey_siewyan@hotmail.com>
2018-11-23 17:08:53 +0100

    update

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ test1=5
 
 if [ "$test1" == "0" ];then
     echo "SKIMMING + SLIMMING"
-    python scripts/nano_postproc.py -c "nTau==0 && Muon_pt[0]>20" -b "keepdropIN.txt" . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
+    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keepdropIN.txt" . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
 elif [ "$test1" == "1" ];then
     echo "SKIMMING + SLIMMING + CLEANING + COMPUTING"
     python scripts/nano_postproc.py -c "nTau==0 && Muon_pt[0]>20" -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.mhtjuProducerCpp mhtju . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
