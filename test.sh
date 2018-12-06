511d659c8c2db6cbba1b4f95122c7d6310780244
SiewYan <siew.yan.hoh@cern.ch>
2018-12-06 18:37:07 +0100

    uniform pd-t2 and local environment

--- test.sh
+++ test.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-test1=4
+test1=5
 
 if [ "$test1" == "0" ];then
-    echo "SKIMMING + SLIMMING"
-    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keepdropIN.txt" . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
+    echo "LOCAL: SKIMMING + SLIMMING"
+    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keep_and_drop_VH.txt" . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
 elif [ "$test1" == "1" ];then
-    echo "SKIMMING + SLIMMING + CLEANING + COMPUTING"
-    python scripts/nano_postproc.py -c "nTau==0 && Muon_pt[0]>20" -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.mhtjuProducerCpp mhtju . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
+    echo "LOCAL: SKIMMING + SLIMMING + CLEANING + COMPUTING"
+    python scripts/nano_postproc.py -c "nTau==0 && Muon_pt[0]>20" -b "keep_and_drop_VH.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.mhtjuProducerCpp mhtju . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/HWplusJ_HToWWTo2L2Nu_WToLNu_M125_13TeV_powheg_pythia8-v1.root
 elif [ "$test1" == "2" ];then
-    echo "SKIMMING + SLIMMING + CLEANING"
-    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.objCleaning cleaning . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
+    echo "LOCAL: SKIMMING + SLIMMING + CLEANING"
+    python scripts/nano_postproc.py -c "Muon_pt[0]>20" -b "keep_and_drop_VH.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.objCleaning cleaning . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
 elif [ "$test1" == "3" ];then
-    echo "SKIMMING + SLIMMING + CLEANING + signal study"
+    echo "LOCAL: SKIMMING + SLIMMING + CLEANING + signal study"
     python python/postprocessing/examples/signalAnalysis.py
 elif [ "$test1" == "4" ];then
-    echo "SKIMMING + SLIMMING + Object Cleaning Study"
-    python scripts/nano_postproc.py -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.cleaningStudy cleaning -E 100001 . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
+    echo "LOCAL: SKIMMING + SLIMMING + Object Cleaning Study"
+    python scripts/nano_postproc.py -b "keep_and_drop_VH.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.cleaningStudy cleaning -E 100001 . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
 elif [ "$test1" == "5" ];then
-    echo "SKIMMING + SLIMMING + PU"
-    python scripts/nano_postproc.py -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer puAutoWeight . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
+    echo "PD-T2: SKIMMING + SLIMMING + Object Cleaning Study"
+    python scripts/nano_postproc.py -b "keep_and_drop_VH.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.cleaningStudy cleaning -E 100001 . /lustre/cmswork/hoh/NANO/SSLep/data/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
 elif [ "$test1" == "6" ];then
-    echo "SKIMMING + SLIMMING + Object Cleaning Study + PU"
-    python scripts/nano_postproc.py -b "keepdropIN.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.cleaningStudy cleaning -I PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer puAutoWeight . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
+    echo "LOCAL: SKIMMING + SLIMMING + PU"
+    python scripts/nano_postproc.py -b "keep_and_drop_VH.txt" -I PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer puAutoWeight . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
+elif [ "$test1" == "7" ];then
+    echo "LOCAL: SKIMMING + SLIMMING + Object Cleaning Study + PU"
+    python scripts/nano_postproc.py -b "keep_and_drop_VH.txt" -I PhysicsTools.NanoAODTools.postprocessing.operational.cleaningStudy cleaning -I PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer puAutoWeight . /Users/shoh/Projects/CMS/PhD/Analysis/SSL/NANOAOD/DYJetsToLL_Pt-400To650_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext2-v1.root
 fi
 
 #cleaningStudy
