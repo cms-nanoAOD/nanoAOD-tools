1954b8b8f1d167a4e56202d725048efa20be1337
SiewYan <siew.yan.hoh@cern.ch>
2019-02-10 16:17:32 +0100

    adding test samplelist

---
+++ batch/filelists/samplelist_testData.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+samplelists=[
+    '/lustre/cmswork/hoh/NANO/SSLep/data/SingleElectronRun2016B-03Feb2017_ver1-v1.root'
+    ]
--- scripts/run_Prod_VH.sh
+++ scripts/run_Prod_VH.sh
@@ -4,9 +4,13 @@ set -e
 DIR=""
 #1 data; 2 mc-short; 3 mc-long; 4 testdata; 5 testmc
 #MODE=3
-samplelist=("2016_Data_VH" "2016_MC_VH_short" "2016_MC_VH_long" "2017_Data_VH")
-queuelist=("local-cms-long" "local-cms-short" "local-cms-long" "local-cms-long")
-prodfolder=("14-Data2016_VH_long" "14-MC_VH_short" "14-MC_VH_long" "14-Data2017_VH_long")
+#samplelist=("2016_Data_VH" "2016_MC_VH_short" "2016_MC_VH_long" "2017_Data_VH")
+#queuelist=("local-cms-long" "local-cms-short" "local-cms-long" "local-cms-long")
+#prodfolder=("14-Data2016_VH_long" "14-MC_VH_short" "14-MC_VH_long" "14-Data2017_VH_long")
+
+samplelist=("testMC" "testData")
+queuelist=("local-cms-short" "local-cms-short")
+prodfolder=("00-testMC" "00-testData")
 
 len=${#samplelist[@]}
 
