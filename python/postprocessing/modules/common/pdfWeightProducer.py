ad7b7cba4cbf6e44b7a4f751d8060f179e157ec6
Christine <camclean@ucdavis.edu>
2019-02-15 07:16:08 -0600

    Added simple pdf weight module

---
+++ python/postprocessing/modules/common/pdfWeightProducer.py
@@ -0,0 +1,40 @@
+import ROOT
+import math, os,re
+import numpy as np
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+class pdfWeightProducer(Module):
+    #def __init__(self):
+
+    #def beginJob(self):
+
+    #def endJob(self):
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        self.out = wrappedOutputTree
+        self.out.branch("LHEPdfWeightUp", "F")
+        self.out.branch("LHEPdfWeightDown", "F")
+                        
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+    
+    def analyze(self, event):
+        """process event, return True (go to next module) or False (fail, go to next event)"""
+ 
+        pdfWeightUp = 1.
+        pdfWeightDown = 1.
+
+        self.pdfWeightList = [  i for i in event.LHEPdfWeight ]
+
+        if len(self.pdfWeightList) > 1:
+            w = np.std(self.pdfWeightList)
+            pdfWeightUp = 1. + abs(w)
+            pdfWeightDown = 1. - abs(w)
+
+        self.out.fillBranch("LHEPdfWeightUp", pdfWeightUp)
+        self.out.fillBranch("LHEPdfWeightDown", pdfWeightDown)
+
+        return True
