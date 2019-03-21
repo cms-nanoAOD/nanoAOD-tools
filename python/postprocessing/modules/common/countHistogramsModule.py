d0d6e03b7a4ea7d7f4285c855c22f2781887b45b
Andrew Levin <amlevin@cern.ch>
2019-03-21 16:50:09 +0100

    automatically detect whether there is a generator weight

--- python/postprocessing/modules/common/countHistogramsModule.py
+++ python/postprocessing/modules/common/countHistogramsModule.py
@@ -11,22 +11,22 @@ class countHistogramsProducer(Module):
     def endJob(self):
         pass
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-        self.h_nevents=ROOT.TH1F('nEvents',   'nEvents',   1, 0, 1)
-        #self.h_nweightedevents=ROOT.TH1F('nWeightedEvents',   'nWeightedEvents',   1, 0, 1)
+        self.h_nevents=ROOT.TH1D('nEvents',   'nEvents',   1, 0, 1)
+        self.h_nweightedevents=ROOT.TH1D('nWeightedEvents',   'nWeightedEvents',   1, 0, 1)
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         prevdir = ROOT.gDirectory
         outputFile.cd()
         self.h_nevents.Write()
-        #self.h_nweightedevents.Write()
+        self.h_nweightedevents.Write()
         prevdir.cd()        
     def analyze(self, event):
         self.h_nevents.Fill(0.5)
-        
-        #if genWeight.genWeight > 0:
-        #    self.h_nweightedevents.Fill(0.5)
-        #else:
-        #    self.h_nweightedevents.Fill(0.5,-1)
+
+        if hasattr(event,'Generator_weight') and event.Generator_weight < 0:
+            self.h_nweightedevents.Fill(0.5,-1)
+        else:    
+            self.h_nweightedevents.Fill(0.5)
 
         return True
 
