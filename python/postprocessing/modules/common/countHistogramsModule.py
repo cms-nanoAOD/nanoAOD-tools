3df8734ca776535e9b2b9e682367a66f72f7b096
Andrew Levin <amlevin@cern.ch>
2019-03-21 19:19:02 +0100

    change name of weighted events histogram

--- python/postprocessing/modules/common/countHistogramsModule.py
+++ python/postprocessing/modules/common/countHistogramsModule.py
@@ -12,21 +12,21 @@ class countHistogramsProducer(Module):
         pass
     def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         self.h_nevents=ROOT.TH1D('nEvents',   'nEvents',   1, 0, 1)
-        self.h_nweightedevents=ROOT.TH1D('nWeightedEvents',   'nWeightedEvents',   1, 0, 1)
+        self.h_neventsgenweighted=ROOT.TH1D('nEventsGenWeighted',   'nEventsGenWeighted',   1, 0, 1)
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         prevdir = ROOT.gDirectory
         outputFile.cd()
         self.h_nevents.Write()
-        self.h_nweightedevents.Write()
+        self.h_neventsgenweighted.Write()
         prevdir.cd()        
     def analyze(self, event):
         self.h_nevents.Fill(0.5)
 
         if hasattr(event,'Generator_weight') and event.Generator_weight < 0:
-            self.h_nweightedevents.Fill(0.5,-1)
+            self.h_neventsgenweighted.Fill(0.5,-1)
         else:    
-            self.h_nweightedevents.Fill(0.5)
+            self.h_neventsgenweighted.Fill(0.5)
 
         return True
 
