8fcba7f7d627e4993c75fdc27e460923ddd00681
SiewYan <siew.yan.hoh@cern.ch>
2019-04-01 23:51:07 +0200

    added function

--- crab/util.sh
+++ crab/util.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 Folder=$1
-
 comd=$2
+curr=$PWD
+dest="/lustre/cmswork/hoh/NANO/SSLep/nanoskim/dataset-crab/2016/v17/"
 
 if [ -z $Folder ] || [ -z $comd ];then
     echo "Select a crab folder or/and option"
@@ -11,6 +12,8 @@ if [ -z $Folder ] || [ -z $comd ];then
     echo -e "\e[91m1 : status\e[0m"
     echo -e "\e[91m2 : getoutput\e[0m "
     echo -e "\e[91m3 : kill\e[0m "
+    echo -e "\e[91m4 : merge\e[0m "
+    echo -e "\e[91m5 : purge\e[0m "
     exit
 fi
 
@@ -18,13 +21,40 @@ echo -e "--- Looking at \e[32m${Folder}\e[0m ---"
 
 for crablet in `ls ${Folder}`
 do
-    echo -e "\e[92m$crablet\e[0m"
-    if [ $comd == 1 ];then
-	action="status"
-    elif [ $comd == 2 ];then
-	action="getoutput"
-    elif [ $comd == 3 ];then
-	action="kill"
+    eval cd ${curr}
+    name=`echo ${crablet} | awk -F 'crab_' '{print $2}'`
+    if (( $comd < 4 ));then
+	echo -e "\e[92m$crablet\e[0m"
+	if [ $comd == 1 ];then
+	    echo "$comd : status of job"
+	    sleep 2
+	    action="status"
+	elif [ $comd == 2 ];then
+	    echo "$comd : getoutput of job"
+	    numoutputfiles=`ls ${Folder}/${crablet}/results/ | wc -l`
+	    if [ $numoutputfiles != 0 ];then
+		echo "roor file retrieved and exist"
+		continue;
+	    fi
+	    action="getoutput"
+	elif [ $comd == 3 ];then
+	    echo "$comd : kill job"
+	    sleep 2
+	    action="kill"
+	fi
+	crab ${action} ${Folder}${crablet}
+    elif [ $comd == 4 ];then
+	echo -e "Mergining root files output in \e[92m$crablet\e[0m"
+	eval cd ${curr}/${Folder}/${crablet}/results
+	if [ -e "${name}.root" ];then
+	    rm ${name}.root
+	fi
+	echo "python ${curr}/../scripts/haddnano.py ${name}.root *"
+	python ${curr}/../scripts/haddnano.py ${dest}/${name}.root *
+	ls
+    elif [ $comd == 5 ];then
+	echo -e "Purging root files output in \e[92m$crablet\e[0m"
+	echo "rm ${curr}/${Folder}/${crablet}/results/*"
+	rm ${curr}/${Folder}/${crablet}/results/${name}.root
     fi
-    crab ${action} ${Folder}${crablet}
 done
