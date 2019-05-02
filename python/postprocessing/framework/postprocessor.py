fb2002ff112ea219b5607671df33952e074041f7
Giovanni <gpetruc@gmail.com>
2019-05-02 15:22:16 +0200

    Support for prefetching files

--- python/postprocessing/framework/postprocessor.py
+++ python/postprocessing/framework/postprocessor.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
 import time
+import hashlib
+import subprocess
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 from PhysicsTools.NanoAODTools.postprocessing.framework.branchselection import BranchSelection
@@ -12,7 +14,8 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.jobreport import JobRepo
 
 class PostProcessor :
     def __init__(self,outputDir,inputFiles,cut=None,branchsel=None,modules=[],compression="LZMA:9",friend=False,postfix=None,
-		 jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None,maxEntries=None):
+		 jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None,maxEntries=None,
+		 prefetch=False,longTermCache=False):
 	self.outputDir=outputDir
 	self.inputFiles=inputFiles
 	self.cut=cut
@@ -36,6 +39,32 @@ class PostProcessor :
         self.histFileName=histFileName
         self.histDirName=histDirName
         self.maxEntries = maxEntries if maxEntries else 9223372036854775807L # 2^63 - 1, largest int64
+        self.prefetch = prefetch
+        self.longTermCache = longTermCache
+    def prefetchFile(self, fname, verbose=True):
+        tmpdir = os.environ['TMPDIR'] if 'TMPDIR' in os.environ else "/tmp"
+        if not fname.startswith("root://"):
+            return fname, False
+        rndchars  = "".join([hex(ord(i))[2:] for i in os.urandom(8)]) if not self.longTermCache else "long_cache-id%d-%s" % (os.getuid(), hashlib.sha1(fname).hexdigest());
+        localfile = "%s/%s-%s.root" % (tmpdir, os.path.basename(fname).replace(".root",""), rndchars)
+        if self.longTermCache and os.path.exists(localfile):
+            if verbose: print "Filename %s is already available in local path %s " % (fname,localfile) 
+            return localfile, False
+        try:
+            if verbose: print "Filename %s is remote, will do a copy to local path %s " % (fname,localfile) 
+            start = time.clock()
+            subprocess.check_output(["xrdcp","-f","-N",fname,localfile])
+            if verbose: print "Time used for transferring the file locally: %s s" % (time.clock() - start) 
+            return localfile, (not self.longTermCache)
+        except:
+            if verbose: print "Error: could not save file locally, will run from remote" 
+            if os.path.exists(localfile):
+                if verbose: print "Deleting partially transferred file %s" % localfile
+                try:
+                    os.unlink(localfile)
+                except:
+                    pass
+            return fname, False
     def run(self) :
         outpostfix = self.postfix if self.postfix != None else ("_Friend" if self.friend else "_Skim")
     	if not self.noOut:
@@ -87,7 +116,11 @@ class PostProcessor :
 	        fname, ffnames = fnames[0], fnames[1:]
 
 	    # open input file
-	    inFile = ROOT.TFile.Open(fname)
+	    if self.prefetch:
+	        ftoread, toBeDeleted = self.prefetchFile(fname)
+	        inFile = ROOT.TFile.Open(ftoread)
+	    else:
+	        inFile = ROOT.TFile.Open(fname)
 
 	    #get input tree
 	    inTree = inFile.Get("Events")
@@ -98,6 +131,8 @@ class PostProcessor :
 	    elist,jsonFilter = preSkim(inTree, self.json, self.cut, maxEntries = self.maxEntries)
 	    if self.justcount:
 		print 'Would select %d entries from %s'%(elist.GetN() if elist else nEntries, fname)
+		if self.prefetch:
+		    if toBeDeleted: os.unlink(ftoread)
 		continue
 	    else:
 		print 'Pre-select %d entries out of %s '%(elist.GetN() if elist else nEntries,nEntries)
@@ -156,6 +191,8 @@ class PostProcessor :
                 print "Done %s" % outFileName
 	    if self.jobReport:
 		self.jobReport.addInputFile(fname,nall)
+	    if self.prefetch:
+		if toBeDeleted: os.unlink(ftoread)
 		
 	for m in self.modules: m.endJob()
 	
