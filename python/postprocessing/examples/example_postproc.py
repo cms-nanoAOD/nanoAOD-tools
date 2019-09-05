e976e233a05c62ba792db6335f0ab4c2050b5cc0
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2019-09-05 16:37:46 +0200

    updates to jmeuncertainty module

--- python/postprocessing/examples/example_postproc.py
+++ python/postprocessing/examples/example_postproc.py
@@ -4,7 +4,8 @@ import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 from importlib import import_module
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
-
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import *
 from  exampleModule import *
-p=PostProcessor(".",["../../../../NanoAOD/test/lzma.root"],"Jet_pt>150","keep_and_drop.txt",[exampleModule()],provenance=True)
+fnames=["/eos/cms/store/mc/RunIISummer16NanoAODv5/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PUMoriond17_Nano1June2019_102X_mcRun2_asymptotic_v7_ext2-v1/120000/FF69DF6E-2494-F543-95BF-F919B911CD23.root"]
+p=PostProcessor(".",fnames,"Jet_pt>150","keep_and_drop.txt",[jetmetUncertainties2016(),exampleModule()],provenance=True)
 p.run()
