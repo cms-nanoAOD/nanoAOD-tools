d75a192d9fa9118c4d066200e66e3b1b8cfd7161
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2019-09-09 09:58:56 +0200

    bugfix;remove keep_drop.txt from example postprc parameter

--- python/postprocessing/examples/example_postproc.py
+++ python/postprocessing/examples/example_postproc.py
@@ -19,6 +19,6 @@ jmeCorrections = createJMECorrector(True, "2016", "B", "Total", True, "AK4PFchs"
 
 fnames=["/eos/cms/store/mc/RunIISummer16NanoAODv5/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/PUMoriond17_Nano1June2019_102X_mcRun2_asymptotic_v7_ext2-v1/120000/FF69DF6E-2494-F543-95BF-F919B911CD23.root"]
 
-#p=PostProcessor(".",fnames,"Jet_pt>150","keep_and_drop.txt",[jetmetUncertainties2016(),exampleModuleConstr()],provenance=True)
-p=PostProcessor(".",fnames,"Jet_pt>150","keep_and_drop.txt",[jmeCorrections(),exampleModuleConstr()],provenance=True)
+#p=PostProcessor(".",fnames,"Jet_pt>150","",[jetmetUncertainties2016(),exampleModuleConstr()],provenance=True)
+p=PostProcessor(".",fnames,"Jet_pt>150","",[jmeCorrections(),exampleModuleConstr()],provenance=True)
 p.run()
