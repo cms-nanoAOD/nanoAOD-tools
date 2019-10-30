1e81f25c75f7a8f9782fb74df9e3ef3c709f87a6
silviodonato <silvio.donato@cern.ch>
2019-10-30 16:47:58 +0100

    add datasetsAndrea, update crab_cfg

--- crab/crab_cfg_all.py
+++ crab/crab_cfg_all.py
@@ -5,7 +5,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 
 config = Configuration()
 
-version = "PROD_4_0"
+version = "PROD_4_1_d"
 
 datasetToTest = [] ## if empty, run on all datasets
 #datasetToTest = ['/DYJetsToLL_M-105To160_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/RunIIAutumn18NanoAODv5-Nano1June2019_102X_upgrade2018_realistic_v19-v1/NANOAODSIM']
@@ -14,6 +14,9 @@ requestsToSkip = [
 ]
 
 requestsToTest = [ ## if empty, run on all datasets
+#"PROD_4_1_others_2017",
+#"PROD_4_1_others_2018",
+#"PROD_4_1_d_W2J_2018AMCPY_ext1",
 #    "PROD_2_0_EWKZ_2018MGHERWIG",
 #    "PROD_2_0_WWJJlnln_2017MGPY",
 #    "PROD_2_0_EWKZ105_2016MGHERWIG",
@@ -58,30 +61,33 @@ print "inputFiles ", config.JobType.inputFiles
 config.JobType.sendPythonFolder         = True
 config.section_("Data")
 #config.Data.inputDataset = '/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAOD-PUMoriond17_94X_mcRun2_asymptotic_v2-v1/NANOAODSIM'
-config.Data.inputDBS = 'global'
-#config.Data.inputDBS = 'phys03'
+#config.Data.inputDBS = 'global'
+config.Data.inputDBS = 'phys03'
 config.Data.splitting = 'FileBased'
 config.Data.allowNonValidInputDataset=True
 #config.Data.splitting = 'Automatic'
 #config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 1
+#config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 5
 #config.Data.totalUnits = 1 ## for testing
 
 config.Data.outLFNDirBase = '/store/user/sdonato/'+version+'/'
 config.Data.publication = True
 config.section_("Site")
+config.Site.ignoreGlobalBlacklist = True
+#config.Data.ignoreLocality = True
 #config.Site.storageSite = "T2_IT_Bari"
 config.Site.storageSite = "T2_IT_Legnaro"
 #config.Site.storageSite = "T2_IT_Pisa"
 
 config.JobType.allowUndistributedCMSSW = True
 
-from datasets2016 import data2016, mc2016
+from datasets2016Andrea import data2016, mc2016
 from datasets2017Andrea import data2017, mc2017
 from datasets2018Andrea import data2018, mc2018
 #from datasetsTest import data2018, mc2018
 
-datasetsNames = ["data2018", "mc2018", "data2017", "mc2017"]
+datasetsNames = ["mc2016","mc2017","mc2018","data2016","data2017","data2018"]
 
 from checker import checkDatasets
 #checkDatasets(datasetsNames, globals())
@@ -96,7 +102,7 @@ if __name__ == '__main__':
         for sample in samples:
             for dataset in datasets[sample]:
                 if type(dataset) != str: continue
-                print "New job"
+#                print "New job"
                 if datasetToTest and not dataset in datasetToTest: continue ## run only datasetToTest, if filled 
                 config.Data.inputDataset = dataset
                 config.Data.lumiMask = None
@@ -113,6 +119,7 @@ if __name__ == '__main__':
                 while requestName in requestNames:
                     requestName = requestName+"_"        
                 requestNames.add(requestName)
+		print requestName
                 if requestName in requestsToSkip: continue
                 if requestsToTest and not requestName in requestsToTest: continue ## run only requestsToTest, if filled 
                 config.General.requestName = requestName
@@ -122,8 +129,9 @@ if __name__ == '__main__':
                 print
                 print config
                 print
-                try:
+#                try:
+                if True:
                     crabCommand('submit', config = config, dryrun = False) ## dryrun = True for local test
                     print "DONE"
-                except:
-                    print('crab submission failed. Move the the next job. %s'%requestName)
+ #               except:
+ #                   print('crab submission failed. Move the the next job. %s'%requestName)
