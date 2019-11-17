65142f2eeeba95627ff8bafa0c30eb792518d485
IzaakWN <iwn_@hotmail.com>
2019-11-17 16:51:14 +0100

    add official TauIDSFTool

---
+++ python/postprocessing/helpers/TauIDSFTool.py
@@ -0,0 +1,83 @@
+import os
+from PhysicsTools.NanoAODTools.postprocessing.tools import ensureTFile, extractTH1
+datapath = os.path.join(os.environ['CMSSW_BASE'],"/src/PhysicsTools/NanoAODTools/data/tau/")
+
+class TauIDSFTool:
+    
+    def __init__(self, year, id, wp='Tight', dm=False, path=datapath):
+        """Choose the IDs and WPs for SFs. For available tau IDs and WPs, check
+        https://github.com/cms-tau-pog/TauIDSFs
+        https://twiki.cern.ch/twiki/bin/viewauth/CMS/TauIDRecommendation13TeV
+        https://cms-nanoaod-integration.web.cern.ch/integration/master-102X/mc102X_doc.html#Tau"""
+        
+        years = ['2016Legacy','2017ReReco','2018ReReco']
+        assert year in years, "You must choose a year from %s."%(', '.join(years))
+        self.ID = id
+        self.WP = wp
+        
+        if id in ['MVAoldDM2017v2','DeepTau2017v2p1VSjet']:
+          if dm:
+            file = ensureTFile("%s/TauID_SF_dm_%s_%s.root"%(path,id,year))
+            self.hist = extractTH1(file,wp)
+            self.hist.SetDirectory(0)
+            file.Close()
+            self.DMs = [0,1,10] if 'oldDM' in id else [0,1,10,11]
+            self.getSFvsPT  = self.disabled
+            self.getSFvsEta = self.disabled
+          else:
+            file = ensureTFile("%s/TauID_SF_pt_%s_%s.root"%(path,id,year))
+            self.func         = { }
+            self.func[None]   = file.Get("%s_cent"%(wp))
+            self.func['Up']   = file.Get("%s_up"%(wp))
+            self.func['Down'] = file.Get("%s_down"%(wp))
+            file.Close()
+            self.getSFvsDM  = self.disabled
+            self.getSFvsEta = self.disabled
+        elif id in ['antiMu3','antiEleMVA6']:
+            file = ensureTFile("%s/TauID_SF_eta_%s_%s.root"%(path,id,year))
+            self.hist = extractTH1(file,wp)
+            self.hist.SetDirectory(0)
+            file.Close()
+            self.genmatches = [1,3] if 'ele' in id.lower() else [2,4]
+            self.getSFvsPT  = self.disabled
+            self.getSFvsDM  = self.disabled
+        else:
+          raise IOError("Did not recognize tau ID '%s'!"%id)
+        
+    def getSFvsPT(self, pt, genmatch=5, unc=None):
+        """Get tau ID SF vs. tau pT."""
+        if genmatch==5:
+          return self.func[unc].Eval(pt)
+        return 1.0
+        
+    def getSFvsDM(self, pt, dm, genmatch=5, unc=None):
+        """Get tau ID SF vs. tau DM."""
+        if dm in self.DMs or pt<40:
+          if genmatch==5:
+            bin = self.hist.GetXaxis().FindBin(dm)
+            SF  = self.hist.GetBinContent(bin)
+            if unc=='Up':
+              SF += self.hist.GetBinError(bin)
+            elif unc=='Down':
+              SF -= self.hist.GetBinError(bin)
+            return SF
+          return 1.0
+        return 0.0
+        
+    def getSFvsEta(self, eta, genmatch, unc=None):
+        """Get tau ID SF vs. tau eta."""
+        eta = abs(eta)
+        if genmatch in self.genmatches:
+          bin = self.hist.GetXaxis().FindBin(eta)
+          SF  = self.hist.GetBinContent(bin)
+          if unc=='Up':
+            SF += self.hist.GetBinError(bin)
+          elif unc=='Down':
+            SF -= self.hist.GetBinError(bin)
+          return SF
+        return 1.0
+        
+    @staticmethod
+    def disabled(*args,**kwargs):
+        raise AttributeError("Disabled method.")
+    
--- python/postprocessing/modules/common/tauCorrProducer.py
+++ python/postprocessing/modules/common/tauCorrProducer.py
@@ -1,91 +1,40 @@
-# Source: https://twiki.cern.ch/twiki/bin/viewauth/CMS/TauIDRecommendation13TeV
 import os
 import ROOT
 import numpy as np
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
-from postprocessors.tauTriggerSFTool import TauTriggerSFTool
+from PhysicsTools.NanoAODTools.postprocessing.helpers.TauIDSFTool import TauIDSFTool
 ROOT.PyConfig.IgnoreCommandLineOptions = True
-
+#datapath = os.environ['CMSSW_BASE']+"/src/PhysicsTools/NanoAODTools/data/tau/"
 
 
 class TauCorrectionsProducer(Module):
     
-    def __init__(self, year, id='MVAoldDM2017v2', WPs=['tight'],
-                             antiEleID='MVAV6',   antiEleWPs=['vloose','tight'],
-                             antiMuID='V3',       antiMuWPs=['loose','tight'],
-                             triggers=None,       eTauTriggerWPs=['tight'], muTauTriggerWPs=['tight'], diTauTriggerWPs=['tight'],
-                             tes=True, tesSys=True):
-        """Choose the IDs and WPs for SFs. For available tau IDs and WPs, check
+    def __init__(self, year, antiJetID='MVAoldDM2017v2', antiJetWPs=['tight'],
+                             antiEleID='antiEleMVA6',    antiEleWPs=['vloose','tight'],
+                             antiMuID='antiMu3',         antiMuWPs=['loose','tight'],
+                             tes=True, tesSys=True, antiJetPerDM=False):
+        """Choose the IDs and WPs for SFs. For available tau IDs, WPs and corrections, check
         https://cms-nanoaod-integration.web.cern.ch/integration/master-102X/mc102X_doc.html#Tau"""
         
-        assert year in [2016,2017,2018], "You must choose a year from 2016, 2017, or 2018."
-        if isinstance(WPs,str):             WPs             = [WPs]
-        if isinstance(antiEleWPs,str):      antiEleWPs      = [antiEleWPs]
-        if isinstance(antiMuWPs,str):       antiMuWPs       = [antiMuWPs]
-        if isinstance(eTauTriggerWPs,str):  eTauTriggerWPs  = [eTauTriggerWPs]
-        if isinstance(muTauTriggerWPs,str): muTauTriggerWPs = [muTauTriggerWPs]
-        if isinstance(diTauTriggerWPs,str): diTauTriggerWPs = [diTauTriggerWPs]
+        years = ['2016Legacy','2017ReReco','2018ReReco']
+        assert year in years, "You must choose a year from %s."%(', '.join(years))
+        if isinstance(antiJetWPs,str): WPs        = [WPs]
+        if isinstance(antiEleWPs,str): antiEleWPs = [antiEleWPs]
+        if isinstance(antiMuWPs,str):  antiMuWPs  = [antiMuWPs]
         
-        # TAU ANTI-JET ID (a.k.a. tau isolation, tau ID)
-        antiJetID   = id
-        antiJetWPs  = WPs
-        antiJetSFs  = { }
-        antiJetBits = { }
-        if antiJetWPs:
-          antiJetSFs_all = {
-            2016: {
-              'MVAoldDM2017v2': {
-                'vvloose': 0.90, 'vloose': 0.90, 'loose': 0.86, 'medium': 0.88, 'tight': 0.87, 'vtight': 0.86, 'vvtight': 0.86 },
-            },
-            2017: {
-              'MVAoldDM2017v2': {
-                'vvloose': 0.89, 'vloose': 0.88, 'loose': 0.89, 'medium': 0.89, 'tight': 0.89, 'vtight': 0.86, 'vvtight': 0.84 },
-            },
-            2018: {
-              'MVAoldDM2017v2': {
-                'vvloose': 0.86, 'vloose': 0.90, 'loose': 0.90, 'medium': 0.90, 'tight': 0.90, 'vtight': 0.89, 'vvtight': 0.89 },
-            },
-          }
-          antiJetBits_all = {
-            'MVAoldDM2017v2': { 'vvloose': 1, 'vloose': 2, 'loose': 4, 'medium': 8, 'tight': 16, 'vtight': 32, 'vvtight': 64 },
-          }
-          assert year in antiJetSFs_all, "You must choose a year from %s"%(antiJetSFs_all.keys())
-          assert antiJetID in antiJetSFs_all[year], "You must choose a tau ID from %s"%(antiJetSFs_all[year].keys())
-          for wp in antiJetWPs:
-            assert wp in antiJetSFs_all[year][antiJetID], "You must choose a %s WP from %s"%(antiJetID,antiJetSFs_all[year][antiJetID].keys())
-            assert wp in antiJetBits_all[antiJetID], "Did not find bit for the %s %s WP, available WPs: %s"%(antiJetID,wp,antiJetBits_all[antiJetID].keys())
-            ###if wp not in antiJetSFs_all[year][antiJetID]: continue
-            antiJetBits[wp] = antiJetBits_all[antiJetID][wp]
-            antiJetSFs[wp]  = antiJetSFs_all[year][antiJetID][wp]
-        
-        # ANTI ELECTRON ID
-        antiEleSFs = { }
-        if not antiEleID:
-          antiEleWPs = [ ]
-        elif antiEleWPs:
-          for wp in antiEleWPs:
-            antiEleSFs[wp] = antiEleSFTool(year,id=antiEleID,wp=wp)
-        
-        # ANTI MUON ID
-        antiMuSFs = { }
-        if not antiMuID:
-          antiMuWPs = [ ]
-        elif antiMuWPs:
-          for wp in antiMuWPs:
-            antiMuSFs[wp] = antiMuSFTool(year,id=antiMuID,wp=wp)
-        
-        # TAU TRIGGER
-        triggerSFs = { }
-        triggerId  = 'MVAv2'
-        for trigger, triggerWPs in [('etau',eTauTriggerWPs),('mutau',muTauTriggerWPs),('ditau',diTauTriggerWPs)]:
-          if trigger in triggers:
-            triggerkey = trigger.replace('e','Ele').replace('mu','Mu').replace('di','Di').replace('tau','Tau')
-            triggerSFs[triggerkey] = { }
-            for wp in triggerWPs:
-              triggerSFs[triggerkey][wp] = TauTriggerSFTool(trigger,wpType=triggerId,tauWP=wp,year=year)
+        antiJetSFs = [ ]
+        antiEleSFs = [ ]
+        antiMuSFs  = [ ]
+        for wp in antiJetWPs:
+          antiJetSFs.append(TauIDSFTool(year,antiJetID,wp,dm=antiJetPerDM))
+        for wp in antiEleWPs:
+          antiEleSFs.append(TauIDSFTool(year,antiEleID,wp))
+        for wp in antiMuWPs:
+          antiMuSFs.append(TauIDSFTool(year,antiMuID,wp))
         
         # TAU ENERGY SCALE
+        # TODO: read from files as well
         tes_vals = { }
         if tes:
           tes_vals = { # units of percentage
@@ -96,19 +45,15 @@ class TauCorrectionsProducer(Module):
           assert year in tes_vals, "You must choose a year from %s"%(tes_vals.keys())
           tes_vals = tes_vals[year]
         
-        self.antiJetID   = antiJetID
-        self.antiJetBit  = "id"+antiJetID # branch with WP bit
-        self.antiJetBits = antiJetBits
-        self.antiJetSFs  = antiJetSFs
-        self.antiEleID   = antiEleID
-        self.antiEleSFs  = antiEleSFs
-        self.antiMuID    = antiMuID
-        self.antiMuSFs   = antiMuSFs
-        self.triggerSFs  = triggerSFs
-        self.triggers    = triggers
-        self.doTES       = tes
-        self.doTESSys    = tesSys and tes
-        self.tes_vals    = tes_vals
+        self.antiJetID  = antiJetID
+        self.antiJetSFs = antiJetSFs
+        self.antiEleID  = antiEleID
+        self.antiEleSFs = antiEleSFs
+        self.antiMuID   = antiMuID
+        self.antiMuSFs  = antiMuSFs
+        self.doTES      = tes
+        self.doTESSys   = tesSys and tes
+        self.tes_vals   = tes_vals
     
     def beginJob(self):
         pass
@@ -120,15 +65,12 @@ class TauCorrectionsProducer(Module):
         """Create branches in output tree."""
         self.out = wrappedOutputTree
         
-        for wp in self.antiJetSFs:
-          self.out.branch("Tau_sf%s_%s"%(self.antiJetID,wp), 'F', lenVar='nTau', title="scale factor for the %s WP of the %s ID"%(wp,self.antiJetID))
-        for wp in self.antiEleSFs:
-          self.out.branch("Tau_sfAntiEle_%s"%wp, 'F', lenVar='nTau', title="scale factor for the %s WP of the anti-electron discriminator"%wp)
-        for wp in self.antiMuSFs:
-          self.out.branch("Tau_sfAntiMu_%s"%wp,  'F', lenVar='nTau', title="scale factor for the %s WP of the anti-muon discriminator"%wp)
-        for trigger in self.triggerSFs:
-          for wp in self.triggerSFs[trigger]:
-            self.out.branch("Tau_sf%sTrig_%s"%(trigger,wp), 'F', lenVar='nTau', title="scale factor for the %s WP of the anti-muon discriminator"%wp)
+        for tool in self.antiJetSFs:
+          self.out.branch("Tau_sf%s_%s"%(tool.ID,tool.WP), 'F', lenVar='nTau', title="scale factor for the %s WP of the %s ID"%(tool.WP,tool.ID))
+        for tool in self.antiEleSFs:
+          self.out.branch("Tau_sf%s_%s"%(tool.ID,tool.WP), 'F', lenVar='nTau', title="scale factor for the %s WP of the %s discriminator"%(tool.WP,tool.ID))
+        for tool in self.antiMuSFs:
+          self.out.branch("Tau_sf%s_%s"%(tool.ID,tool.WP), 'F', lenVar='nTau', title="scale factor for the %s WP of the %s discriminator"%(tool.WP,tool.ID))
         
         if self.doTES:
           self.out.branch("Tau_pt_corr",         'F', lenVar='nTau', title="tau pT, corrected with the tau energy scale")
@@ -147,10 +89,9 @@ class TauCorrectionsProducer(Module):
         """Process event, return True (pass, go to next module) or False (fail, go to next event)."""
         
         # BRANCH VALUES
-        taus_antiJetSF_corr    = { wp: [ 1. ]*event.nTau for wp in self.antiJetSFs }
-        taus_antiEleSF_corr    = { wp: [ 1. ]*event.nTau for wp in self.antiEleSFs }
-        taus_antiMuSF_corr     = { wp: [ 1. ]*event.nTau for wp in self.antiMuSFs  }
-        taus_triggerSF_corr    = { tr: { wp: [ 1. ]*event.nTau for wp in self.triggerSFs[tr] } for tr in self.triggerSFs }
+        taus_antiJetSFs         = { t: [ 1. ]*event.nTau for t in self.antiJetSFs }
+        taus_antiEleSFs         = { t: [ 1. ]*event.nTau for t in self.antiEleSFs }
+        taus_antiMuSFs          = { t: [ 1. ]*event.nTau for t in self.antiMuSFs  }
         if self.doTES:
           taus_pt_corr         = [ ]
           taus_mass_corr       = [ ]
@@ -163,7 +104,6 @@ class TauCorrectionsProducer(Module):
         # LOOP over TAUS
         taus = Collection(event, 'Tau')
         for i, tau in enumerate(taus):
-          dm = tau.decayMode
           
           # UNCORRECTED PT / MASS
           if self.doTES:
@@ -177,9 +117,9 @@ class TauCorrectionsProducer(Module):
           
           # REAL TAU
           if tau.genPartFlav==5:
-            for wp in taus_antiJetSF_corr:
-              if self.antiJetBits[wp]<getattr(tau,self.antiJetBit):
-                taus_antiJetSF_corr[wp][i] = self.antiJetSFs[wp]
+            dm = tau.decayMode
+            for tool in taus_antiJetSFs:
+              taus_antiJetSFs[tool][i] = tool.getSFvsPT(tau.pt)
             if self.doTES and dm in self.tes_vals:
               tes                      = 1. + self.tes_vals[dm][0]/100.
               taus_pt_corr[i]         *= tes
@@ -191,32 +131,23 @@ class TauCorrectionsProducer(Module):
                 taus_mass_corrUp[i]   *= tesUp
                 taus_mass_corrDown[i] *= tesDown
           
-          # ELECTRON -> TAU FAKE
+          # ELECTRON -> TAU
           elif tau.genPartFlav in [1,3]:
-            for wp in taus_antiEleSF_corr:
-              taus_antiEleSF_corr[wp][i] = self.antiEleSFs[wp].getSF(tau)
+            for tool in taus_antiEleSFs:
+              taus_antiEleSFs[wp][i] = self.antiEleSFs[wp].getSF(tau.eta,tau.genPartFlav)
           
-          # MUON -> TAU FAKE
+          # MUON -> TAU
           elif tau.genPartFlav in [2,4]:
-            for wp in taus_antiMuSF_corr:
-              taus_antiMuSF_corr[wp][i] = self.antiMuSFs[wp].getSF(tau)
-          
-          # TRIGGER SF
-          if dm in [0,1,10]:
-            for trigger in self.triggerSFs:
-              for wp in self.triggerSFs[trigger]:
-                taus_triggerSF_corr[trigger][wp][i] = self.triggerSFs[trigger][wp].getTriggerScaleFactor(tau.pt,tau.eta,tau.phi,dm)
+            for tool in taus_antiMuSFs:
+              taus_antiMuSFs[wp][i] = tool.getSFvsEta(tau.eta,tau.genPartFlav)
         
         # FILL BRANCHES
-        for wp in taus_antiJetSF_corr:
-          self.out.fillBranch("Tau_sf%s_%s"%(self.antiJetID,wp), taus_antiJetSF_corr[wp])
-        for wp in taus_antiEleSF_corr:
-          self.out.fillBranch("Tau_sfAntiEle_%s"%wp, taus_antiEleSF_corr[wp])
-        for wp in taus_antiMuSF_corr:
-          self.out.fillBranch("Tau_sfAntiMu_%s"%wp,  taus_antiMuSF_corr[wp])
-        for trigger in self.triggerSFs:
-          for wp in self.triggerSFs[trigger]:
-            self.out.fillBranch("Tau_sf%sTrig_%s"%(trigger,wp), taus_triggerSF_corr[trigger][wp])
+        for tool in taus_antiJetSFs:
+          self.out.fillBranch("Tau_sf%s_%s"%(tool.ID,tool.WP),taus_antiJetSFs[tool])
+        for tool in taus_antiEleSFs:
+          self.out.fillBranch("Tau_sf%s_%s"%(tool.ID,tool.WP),taus_antiEleSFs[tool])
+        for tool in taus_antiMuSFs:
+          self.out.fillBranch("Tau_sf%s_%s"%(tool.ID,tool.WP),taus_antiMuSFs[tool])
         if self.doTES:
           self.out.fillBranch("Tau_pt_corr",         taus_pt_corr)
           self.out.fillBranch("Tau_mass_corr",       taus_mass_corr)
@@ -230,111 +161,7 @@ class TauCorrectionsProducer(Module):
     
 
 
-class antiMuSFTool:
-    
-    def __init__(self, year, wp='tight', id='anitMuV3'):
-        """Initialize tool to get anti-muon discriminator SFs."""
-        etabins      = ( 0.4, 0.8, 1.2, 1.7, 2.3 )
-        antiMuWP_all = {
-          2016: {
-            'V3': { 'loose': (1.146, 1.084, 1.218, 1.490, 2.008),
-                    'tight': (1.470, 1.367, 1.251, 1.770, 1.713), },
-          },
-          2017: {
-            'V3': { 'loose': (1.061, 1.022, 1.097, 1.030, 1.941),
-                    'tight': (1.170, 1.290, 1.140, 0.930, 1.610), },
-          },
-          2018: {
-            'V3': { 'loose': (1.05,  0.96,  1.06,  1.45,  1.75 ),
-                    'tight': (1.23,  1.37,  1.12,  1.84,  2.01 ), },
-          },
-        }
-        antiMuWP_bits = {
-          'V3': { 'loose': 1, 'tight': 2 },
-        }
-        assert year in antiMuWP_all, "You must choose a year from %s"%(antiMuWP_all.keys())
-        assert id in antiMuWP_all[year], "You must choose an antiMu ID from %s"%(antiMuWP_all[year].keys())
-        assert wp in antiMuWP_all[year][id], "You must choose an antiMu %s %s WP from %s"%(id,wp,antiMuWP_all[year][id].keys())
-        assert id in antiMuWP_bits and wp in antiMuWP_bits[id], "No bit found for antiMu %s %s WP!"%(id,wp)
-        assert len(etabins)==len(antiMuWP_all[year][id][wp]),\
-          "Number of bins (%s) must be the same as the number of SFs (%s)."%(len(etabins),len(antiMuWP_all[year][id][wp]))
-        
-        self.etabins = etabins
-        self.SFs     = antiMuWP_all[year][id][wp]
-        self.WP      = wp
-        self.bit     = "idAntiMu" if id=='V3' else "id"+id
-        self.wpbit   = antiMuWP_bits[id][wp]
-    
-    def getSF(self,tau):
-      """Get the SF for a given eta."""
-      abseta = abs(tau.eta)
-      if getattr(tau,self.bit)<self.wpbit:
-        return 0.0
-      for etamax, sf in zip(self.etabins,self.SFs):
-        if abseta < etamax:
-          return sf
-      return 1.
-    
-
-
-class antiEleSFTool:
-    
-    def __init__(self, year, wp='tight', id='MVAV6'):
-        """Initialize tool to get anti-electron discriminator SFs."""
-        etabins   = ( 1.460, 1.558 )
-        antiEleWP_all = {
-          2016: {
-            'MVAV6': { 'vloose': ( 1.317,  1.547 ),
-                       'loose':  ( 1.466,  1.719 ),
-                       'medium': ( 1.502,  1.594 ),
-                       'tight':  ( 1.486,  1.560 ),
-                       'vtight': ( 1.601,  1.401 ), },
-          },
-          2017: {
-            'MVAV6': { 'vloose': ( 1.09,   1.19  ),
-                       'loose':  ( 1.17,   1.25  ),
-                       'medium': ( 1.40,   1.21  ),
-                       'tight':  ( 1.80,   1.53  ),
-                       'vtight': ( 1.96,   1.66  ), },
-          },
-          2018: {
-            'MVAV6': { 'vloose': ( 1.130,  1.003 ), # PRELIMINARY
-                       'loose':  ( 1.229,  0.926 ),
-                       'medium': ( 1.360,  0.910 ),
-                       'tight':  ( 1.460,  1.020 ),
-                       'vtight': ( 1.560,  1.030 ), },
-          },
-        }
-        antiEleWP_bits = {
-          'MVAV6': { 'vloose': 1, 'loose': 2, 'medium': 4, 'tight': 8, 'vtight': 16 },
-        }
-        assert year in antiEleWP_all, "You must choose a year from %s"%(antiEleWP_all.keys())
-        assert id in antiEleWP_all[year], "You must choose an antiEle ID from %s"%(antiEleWP_all[year].keys())
-        assert wp in antiEleWP_all[year][id], "You must choose an antiEle %s %s WP from %s"%(id,wp,antiEleWP_all[year][id].keys())
-        assert id in antiEleWP_bits and wp in antiEleWP_bits[id], "No bit found for antiEle %s %s WP!"%(id,wp)
-        assert len(etabins)==len(antiEleWP_all[year][id][wp]),\
-          "Number of bins (%s) must be the same as the number of SFs (%s)."%(len(etabins),len(antiEleWP_all[year][id][wp]))
-        
-        self.etabins = etabins
-        self.SFs     = antiEleWP_all[year][id][wp]
-        self.WP      = wp
-        self.bit     = "idAntiEle" if id=='MVAV6' else "id"+id
-        self.wpbit   = antiEleWP_bits[id][wp]
-    
-    def getSF(self,tau):
-      """Get the SF for a given eta."""
-      abseta = abs(tau.eta)
-      if getattr(tau,self.bit)<self.wpbit:
-        return 0.0
-      if abseta < self.etabins[0]:
-        return self.SFs[0]
-      elif abseta > self.etabins[1]:
-        return self.SFs[1]
-      return 1.
-    
-
-
 # DEFINE modules to avoid having them loaded when not needed
-tauCorrs2016 = lambda: TauCorrectionsProducer(2016)
-tauCorrs2017 = lambda: TauCorrectionsProducer(2017)
-tauCorrs2018 = lambda: TauCorrectionsProducer(2018)
+tauCorrs2016Legacy = lambda: TauCorrectionsProducer('2016Legacy')
+tauCorrs2017ReReco = lambda: TauCorrectionsProducer('2017ReReco')
+tauCorrs2018ReReco = lambda: TauCorrectionsProducer('2018ReReco')
