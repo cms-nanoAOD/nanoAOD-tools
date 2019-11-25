3cf368d17fbab373c8e8f34a31c8cf00887749e2
daiktas <vilius.cepaitis@gmail.com>
2019-11-25 11:09:27 +0000

    init

---
+++ test/modules/__init__.py
@@ -0,0 +1,32 @@
+import ROOT
+import sys
+
+#can only load this once
+if (ROOT.gSystem.Load("libPhysicsToolsNanoAODTools.so")!=0):
+    print "Cannot load 'libPhysicsToolsNanoAODTools'"
+    sys.exit(1)
+
+from MuonSelection import MuonSelection
+from MuonVeto import MuonVeto
+from SingleMuonTriggerSelection import SingleMuonTriggerSelection
+from SignalTriggerSelection import SignalTriggerSelection
+from JetMetUncertainties import JetMetUncertainties
+from JetSelection import JetSelection
+from TaggerEvaluation import TaggerEvaluation
+from EventSkim import EventSkim
+from EventObservables import EventObservables
+from MetFilter import MetFilter
+from PileupWeight import PileupWeight
+from TaggerWorkingpoints import TaggerWorkingpoints
+from EventInfo import EventInfo
+from ElectronVeto import ElectronVeto
+from DataFlag import DataFlag
+from EventDump import EventDump
+from InvariantSystem import InvariantSystem
+from JetTruthFlags import JetTruthFlags
+from JetTaggerResult import JetTaggerResult
+from PDFWeights import PDFWeights
+from WNLOWeights import WNLOWeights
+from JetFeatures import JetFeatures
+from LegacyTagger import LegacyTagger
+from LepJetFinder import LepJetFinder
