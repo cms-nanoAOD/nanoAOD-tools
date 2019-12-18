34b991cf3ed0c9002907ac2c8add4bd1521f0af2
Andrea Piccinelli <andrea8395@gmail.com>
2019-12-18 12:41:59 +0100

    Reconstruction of the 4momentum of a top quark decaying in semileptonic final states

--- python/postprocessing/topreco.py
+++ python/postprocessing/topreco.py
@@ -17,18 +17,65 @@ class TopUtilities():
         
         MisET2 = (metPx**2. + metPy**2.)
         mu = (mW**2.)/2. + metPx*leptonPx + metPy*leptonPy
-        a = (mu*leptonPz) / (leptonE**2. - leptonPz**2.)
+        a = (mu*leptonPz) / (TMath.Power(leptonE, 2.) - TMath.Power(leptonPz, 2.))
         a2 = TMath.Power(a, 2.)
+        b = TMath.Power(leptonE, 2.)*(MisET2) - TMath.Power(mu, 2.)/TMath.Power(leptonE, 2.) - TMath.Power(leptonPz, 2.)
+
+        p4nu_rec = root.TLorentzVector()
+        p4W_rec = root.TLorentzVector()
+        p4b_rec = root.TLorentzVector()
+        p4Top_rec = root.TLorentzVector()
+        p4lep_rec = root.TLorentzVector()
+        neutrino = root.TLorentzVector()
+
+        p4lep_rec.SetPxPyPzE(leptonPx, leptonPy, leptonPz, leptonE)
+        p40_rec = root.TLorentzVector(0.0, 0.0, 0.0, 0.0)
+
+        if (a2-b) > 0:
+            root = TMath((a2-b), 0.5)
+            pz1 = a + root
+            pz2 = a - root
+            nNuSol = 2
+            pznu = 0.0
+
+            if abs(pz1) > abs(pz2):
+                pznu = pz2
+            else:
+                pznu = pz1
+
+            Enu = TMath.Power((MisET2 + pznu**2), 0.5)
+            p4nu_rec.SetPxPyPzE(metPx, metPy, pznu, Enu)
+            neutrino = p4nu_rec
+
+        else:
+            ptlep = leptonPt
+            pxlep = leptonPx
+            pylep = leptonPy
+            metpx = metPx
+            metpy = metPy
+
+            EquationCoeff = {'a': 1,
+                             'b': (-3 * pylep * mW / ptlep),
+                             'c': (((mW**2.) * (2. * pylep**2.) / (ptlep**2)) + mW**2. - (4. * pxlep**3. * metpx / ptlep**2) - (4. * pxlep**2. * pylep * metpy / ptlep**2)),
+                             'd': ((4. * pxlep**2. * mW * metpy / ptlep) - pylep * mW**3. / ptlep)}
+        
+        return neutrino
 
     def top4Momentum(self, lepton, jet, metPx, metPy):
-        neutrino = self.NuMomentum(lepton.Px(), lepton.Py(), lepton.Pz(), lepton.Pt(), lepton.E, metPx, metPy)
+        leptonPx = lepton.Px()
+        leptonPy = lepton.Py()
+        leptonPz = lepton.Pz()
+        leptonPt = lepton.Pt()
+        leptonE = lepton.Energy()
+        neutrino = self.NuMomentum(leptonPx, leptonPy, leptonPz, leptonPt, leptonE, metPx, metPy)
         top = lepton + jet + neutrino
+        #print top.Pt(), top.Eta(), top.Phi(), top.E()
         return top
 
-reco = TopUtilities()
-lep = root.TLorentzVector(1.0, 5.0, 6.0, 9.)
-jet = root.TLorentzVector(-5.0, 17.0, -8.0, 5.)
-metPx = 9.
-metPy = 10.
+'''reco = TopUtilities()
+lep = root.TLorentzVector(1.04, 5.08, 6.07, 9.56)
+jet = root.TLorentzVector(-5.03, 17.07, -8.06, 5.98)
+metPx = 9.45
+metPy = 10.67
 
-#reco.top4Momentum(lep, jet, metPx, metPy)
+vector = reco.top4Momentum(lep, jet, metPx, metPy)'''
