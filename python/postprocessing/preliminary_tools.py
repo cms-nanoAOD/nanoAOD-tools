ab054f267c059c8ce0756a8e8da8ccf2ad6f0c6f
Andrea Piccinelli <andrea8395@gmail.com>
2020-02-21 09:27:31 +0100

    tools for reco

---
+++ python/postprocessing/preliminary_tools.py
@@ -0,0 +1,136 @@
+import ROOT
+import math
+from array import array
+from PhysicsTools.NanoAODTools.postprocessing.tools import *
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object, Event
+from PhysicsTools.NanoAODTools.postprocessing.framework.treeReaderArrayTools import *
+from PhysicsTools.NanoAODTools.postprocessing.framework.preskimming import preSkim
+'''
+inputpath = "/eos/home-a/adeiorio/Wprime/nosynch/" 
+inpfiles = [#"Wprime_4000_RH"
+            ,"TT_Mtt-700to1000"
+            #"WJets",
+            #,"QCD_Pt_600to800_1"
+            #"SingleMuon_Run2016G_1"
+        ]
+'''
+#ROOT.gROOT.SetStyle('Plain')
+#ROOT.gStyle.SetPalette(1)
+ROOT.gStyle.SetOptStat(0)
+ROOT.gROOT.SetBatch()        # don't pop up canvases                                                                                                                                              
+ROOT.TH1.SetDefaultSumw2()
+ROOT.TGaxis.SetMaxDigits(3)
+
+colors = [ROOT.kBlue,
+          ROOT.kBlack,
+          ROOT.kRed,
+          ROOT.kGreen+2,
+          ROOT.kMagenta+2,
+          ROOT.kAzure+6
+]
+
+def pass_MET(flag): #returns the True if the event pass the MET Filter requiriments otherwise False
+    return flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter
+
+def get_Mu(muons): #returns a collection of muons that pass the selection performed by the filter function
+    return list(filter(lambda x : x.tightId and abs(x.eta) < 2.4, muons))
+
+def get_LooseMu(muons): #returns a collection of muons that pass the selection performed by the filter function
+    return list(filter(lambda x : x.looseId and not x.tightId and x.pt > 35 and abs(x.eta) < 2.4, muons))
+
+def get_Ele(electrons): #returns a collection of electrons that pass the selection performed by the filter function
+    return list(filter(lambda x : x.mvaFall17V2noIso_WP90 and ((abs(x.eta) < 1.4442) or (abs(x.eta) > 1.566 and abs(x.eta) < 2.5)), electrons))
+
+def get_LooseEle(electrons): #returns a collection of electrons that pass the selection performed by the filter function
+    return list(filter(lambda x : x.mvaFall17V2noIso_WPL and not x.mvaFall17V2noIso_WP90 and x.pt > 35 and ((abs(x.eta) < 1.4442) or (abs(x.eta) > 1.566 and abs(x.eta) < 2.5)), electrons))
+
+def get_Jet(jets, pt): #returns a collection of jets that pass the selection performed by the filter function
+    return list(filter(lambda x : x.jetId >= 2 and abs(x.eta) < 2.4 and x.pt > pt, jets))
+
+def get_HT(jets):
+    HT = ROOT.TLorentzVector()
+    for jet in jets:
+        HT += jet.p4()
+    return HT
+ 
+def presel(PV, muons, electrons, jets): #returns three booleans: goodEvent assure the presence of at least a good lepton vetoing the presence of additional loose leptons, goodMuEvt is for good muon event, goodEleEvt is for good muon event   
+    isGoodPV = (PV.ndof>4 and abs(PV.z)<20 and math.hypot(PV.x, PV.y)<2) #basic requirements on the PV's goodness
+    VetoMu = get_LooseMu(muons)
+    goodMu = get_Mu(muons)
+    VetoEle = get_LooseEle(electrons)
+    goodEle = get_Ele(electrons)
+    goodJet = get_Jet(jets, 25)
+
+    isGoodEvent = ((((len(goodMu) >= 1) and (len(goodEle) == 0)) or ((len(goodMu) == 0) and (len(goodEle) >= 1))) and len(VetoMu) == 0 and len(VetoEle) == 0 and len(goodJet) >= 2)
+    isMuon = (len(goodMu) >= 1) and (len(goodEle) == 0) and len(VetoMu) == 0 and len(VetoEle) == 0 and len(goodJet) >= 2
+    isElectron = (len(goodMu) == 0) and (len(goodEle) >= 1) and len(VetoMu) == 0 and len(VetoEle) == 0 and len(goodJet) >= 2
+    goodEvent = isGoodPV and isGoodEvent
+    goodMuEvt = isGoodPV and isMuon
+    goodEleEvt = isGoodPV and isElectron
+    return goodEvent, goodMuEvt, goodEleEvt
+
+def print_hist(infile, hist, option = "HIST", log = False):
+    plotpath = "./plots/"
+    if not(isinstance(hist, list)):
+        c1 = ROOT.TCanvas(infile + "_" + hist.GetName(), "c1", 50,50,700,600)
+        if log:
+            c1.SetLogy()
+        hist.Draw(option)            
+        c1.Print(plotpath + infile + "_" + hist.GetName() + ".png")
+        c1.Print(plotpath + infile + "_" + hist.GetName() + ".root")
+    else:
+        c1 = ROOT.TCanvas('comparison', "c1", 50,50,700,600)#infile + "_" + hist[0].GetName() + '_comparison', "c1", 50,50,700,600)
+        if isinstance(hist[0], ROOT.TGraph) or isinstance(hist[0], ROOT.TGraphAsymmErrors):
+            i = 0
+            mg = ROOT.TMultiGraph()
+            for h in hist:
+                h.SetLineColor(colors[i])
+                mg.Add(h)
+                i += 1
+            cap = hist[0].GetXaxis().GetTitle()
+            mg.Draw(option)
+            mg.GetXaxis().SetTitle(hist[0].GetXaxis().GetTitle())
+            mg.GetYaxis().SetTitle(hist[0].GetYaxis().GetTitle())
+
+            Low = hist[0].GetXaxis().GetBinLowEdge(1)
+            Nbin = hist[0].GetXaxis().GetNbins()
+            High = hist[0].GetXaxis().GetBinUpEdge(Nbin)
+            mg.GetXaxis().Set(Nbin, Low, High)
+            
+            for i in range(hist[0].GetXaxis().GetNbins()):
+                u = i + 1
+                mg.GetXaxis().SetBinLabel(u, hist[0].GetXaxis().GetBinLabel(u))
+        elif isinstance(hist[0], ROOT.TEfficiency):
+            i = 0
+            mg = ROOT.TMultiGraph()
+            for h in hist:
+                h.SetLineColor(colors[i])
+                mg.Add(h.CreateGraph())
+                i += 1
+            mg.Draw(option)
+            mg.GetXaxis().SetTitle(hist[0].GetXaxis().GetTitle())
+            mg.GetYaxis().SetTitle(hist[0].GetYaxis().GetTitle())
+        elif isinstance(hist[0], ROOT.TH1F):
+            mg = ROOT.THStack()
+            i = 0
+            for h in hist:
+                h.SetLineColor(colors[i])
+                mg.Add(h)
+                i += 1
+            mg.Draw(option)
+            mg.GetXaxis().SetTitle(hist[0].GetXaxis().GetTitle())
+            mg.GetYaxis().SetTitle(hist[0].GetYaxis().GetTitle())
+        else:
+            for h in hist:
+                h.Draw(option+'SAME')
+        c1.BuildLegend()
+        c1.Modified()
+        c1.Update()
+        c1.Print(plotpath + 'comparison' + ".png")#plotpath + infile + "_" + hist[0].GetName() + '_comparison' + ".png")
+        c1.Print(plotpath + 'comparison' + ".root")#plotpath + infile + "_" + hist[0].GetName() + '_comparison' + ".root")
+
+def save_hist(infile, hist, option = "HIST"):
+     fout = ROOT.TFile.Open("./plots/"+ infile +".root", "UPDATE")
+     fout.cd()
+     hist.Write()
+     fout.Close()
