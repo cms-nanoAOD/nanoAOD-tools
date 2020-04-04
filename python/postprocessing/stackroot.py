0e3711cd194bc2e7bbdc36b25bae5c185926eda9
Andrea Piccinelli <andrea8395@gmail.com>
2020-04-04 18:32:26 +0200

    drawing plots pre-stacking

--- python/postprocessing/stackroot.py
+++ python/postprocessing/stackroot.py
@@ -24,12 +24,12 @@ class variabile(object):
         return  '\"'+str(self._name)+'\",\"'+str(self._title)+'\",'+str(self._nbins)+','+str(self._xmin)+','+str(self._xmax)
 
 def cutToTag(cut):
-    newstring = cut.replace(">=","_GE_").replace(">","_G_").replace(" ","").replace("&&","_AND_").replace("||","_OR_").replace("<=","_LE_").replace("<","_L_").replace(".","p").replace("(","").replace(")","").replace("==","_EQ_").replace("=","_EQ_").replace("*","_AND_").replace("+","_OR_")
+    newstring = cut.replace(">=","_GE_").replace(">","_G_").replace(" ","").replace("&&","_AND_").replace("||","_OR_").replace("<=","_LE_").replace("<","_L_").replace(".","p").replace("((", "[").replace("))", "]").replace("(","").replace(")","").replace("==","_EQ_").replace("=","_EQ_").replace("*","_AND_").replace("+","_OR_")
     return newstring
 
 lumi = {
     "2016": 35.9,
-    "2017": 41.53,
+    "2017": 4.82,#41.53, <- overall integrated luminosity!
     "2018": 57.3,
 }
 
@@ -52,53 +52,64 @@ findplots.close()
 for sample in samples:
     sampledir = inputpath + sample[0] + "/"
     rootfiles = os.listdir(sampledir)
-
+    print "Sample: " + sampledir
     sobj = "losamples."+sample[0]
     x = "scard"
     exec("%s = %s" % (x, sobj))
 
+    histos = []
+
+    for plot in plots:
+        var = None
+        hlist = None
+        name = "h_" + plot[0]
+        title = plot[0]
+        cut = None
+        if plot[1] == 'nocut':
+            cut = ""
+        else:
+            cut = plot[1]
+            name = name + "_" + cutToTag(cut)
+        nbins = int(float(plot[2]))
+        nmin = float(plot[3])
+        nmax = float(plot[4])
+        var = variabile(name, title, cut, nbins, nmin, nmax)
+        hlist = []
+        hlist.append(copy.deepcopy(var))
+        hlist.append(ROOT.TH1F(name, title, nbins, nmin, nmax))
+        histos.append(copy.deepcopy(hlist))
+        hlist = None
+
     for k, rootfile in enumerate(rootfiles):
         if k > 0:
             break
+        print "\trootfile #%i" % (k)
         infile = ROOT.TFile.Open(sampledir + rootfile)        
         tree = InputTree(infile.Events)
         nentries = tree.GetEntries()
-        histos = []
-
-        for plot in plots:
-            var = None
-            hlist = None
-
-            name = "h_" + plot[0]
-            title = plot[0]
-            cut = None
-            if plot[1] == 'nocut':
-                cut = ""
-            else:
-                cut = plot[1]
-                name = name + "_" + cutToTag(cut)
-            nbins = int(plot[2])
-            nmin = float(plot[3])
-            nmax = float(plot[4])
-            var = variabile(name, title, cut, nbins, nmin, nmax)
-            hlist = []
-            hlist.append(copy.deepcopy(var))
-            hlist.append(ROOT.TH1F(name, title, nbins, nmin, nmax))
-            histos.append(copy.deepcopy(hlist))
-            hlist = None
-
+        #for h in histos:
+            #print h[1], h[1].GetEntries()
+        
         for i in xrange(0, nentries):
+            
+            if (i % int(nentries/20)) == 0:
+                perc = float(i)/float(nentries) * 100.
+                sys.stdout.write("\r\t\tPercentage wrt total: {0}%".format(math.ceil(perc)))
+                sys.stdout.flush()
             event = Event(tree,i)
             electrons = Collection(event, "Electron")
             muons = Collection(event, "Muon")
-            goodMu = get_Mu(muons)
-            goodEle = get_Ele(electrons)
+            jets = Collection(event, "Jet")
+            PV = Object(event, "PV")
+            #goodMu = get_Mu(muons)
+            #goodEle = get_Ele(electrons)
+            goodEvt, isMu, isEle = presel(PV, muons, electrons, jets)
 
             if sample[1] == 'mu' or sample[1] == 'muon':
-                if not (len(goodMu) > 0 and len(goodEle) == 0):
+                if not isMu:#(len(goodMu) > 0 and len(goodEle) == 0):
                     continue
             elif sample[1] == 'ele' or sample[1] == 'electron':
-                if not (len(goodMu) == 0 and len(goodEle) > 0):
+                if not isEle:#(len(goodMu) == 0 and len(goodEle) > 0):
                     continue
 
             for h in histos:
@@ -110,7 +121,12 @@ for sample in samples:
                 h[1].Add(htemp)
                 htemp = None
 
+        infile.Close()
+        print "rootfile closed"
+
+    if 'Data' not in sample[0]:
         for h in histos:
+            print "\tBefore scaling: " + str(h[1].GetEntries())
             scalefac = 1. / h[1].GetEntries()
             for k, v in lumi.items():
                 if k in rootfile:
@@ -118,11 +134,11 @@ for sample in samples:
                     break
             scalefac = scalefac * scard.sigma * 1000.
             h[1].Scale(scalefac)
-            
-        outname = sample[0]+'_'+sample[1]
-        print sample[0], sample[1]#outname
-        save_hist(outname, "Plots", h[1])
-        infile.Close()
+            print "\tAfter scaling: " + str(h[1].GetEntries())
+    outname = sample[0]+'_'+sample[1]
+    print "\tSaving " + outname#sample[0], sample[1]#outname
+    save_hist(outname, "Plots", h[1])
+
         
 
 '''
