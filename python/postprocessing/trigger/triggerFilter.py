bd106fc23cec7c7d79a7ddc3704b7aa79455cb13
rymuelle <rmueller@physics.tamu.edu>
2021-05-12 19:09:54 +0200

    trigger module

---
+++ python/postprocessing/trigger/triggerFilter.py
@@ -0,0 +1,28 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+class triggerFilter(Module):
+
+    def __init__(self, triggers):
+        self.triggers = triggers
+        pass
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        list_of_branches = wrappedOutputTree.tree().GetListOfBranches()
+        self._triggers = [trigger for trigger in self.triggers if trigger in list_of_branches]
+
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+
+    def analyze(self, event):
+        HLT_select = False
+        for trigger in self._triggers:
+            if event[trigger]: 
+                HLT_select = 1
+                break
+        if not HLT_select: return False
+        else: return True
+
