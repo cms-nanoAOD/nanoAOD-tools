08d2d586ef2615b56c650b49650e1ee36d28cbf3
rymuelle <rmueller@physics.tamu.edu>
2021-05-13 02:40:29 +0200

    2018 crab dir

---
+++ crab_bff/2018/cfg/ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-_cfg.py
@@ -0,0 +1,30 @@
+from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_('General')
+config.General.requestName = 'ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-'
+config.General.transferLogs = True
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.scriptExe = 'crab_script_mc.sh'
+# hadd nano will not be needed once nano tools are in cmssw
+config.JobType.inputFiles = ['job_2018_mc_crab.py', '../../scripts/haddnano.py', 'keep_and_drop_bff.txt']
+config.JobType.sendPythonFolder = True
+config.section_('Data')
+config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8/RunIIAutumn18NanoAODv7-Nano02Apr2020_102X_upgrade2018_realistic_v21_ext1-v1/NANOAODSIM'
+#config.Data.inputDBS = 'phys03'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 2
+
+config.Data.outLFNDirBase = '/store/group/phys_exotica/bffZprime/nanoAODskimmed/2018'
+config.Data.publication = False
+config.Data.outputDatasetTag = 'ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-'
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+# config.section_('User')
+#config.User.voGroup = 'dcms'
+config.JobType.allowUndistributedCMSSW = True
---
+++ crab_bff/2018/make_cfg.py
@@ -0,0 +1,25 @@
+from crab_cfg_mc import template
+from crab_cfg_data import template_data
+import re
+
+with open('data.list', 'r') as f:
+    for fname in f.readlines():
+        post = "{}_{}".format(fname.split('/')[1], fname.split('/')[2])
+        crab_dict = {
+       'NanoPost1':post,
+       'NanoTestPost':post,
+       'das':fname.replace('\n','')} 
+
+        #with open('cfg/'+post+'_cfg.py', 'w') as f2:
+        #    f2.write(template_data.format(**crab_dict))
+
+with open('mc.list', 'r') as f:
+    for fname in f.readlines():
+        post = "{}_{}".format(fname.split('/')[1], fname.split('/')[2])[:50]
+        crab_dict = {
+        'NanoPost1':post,
+        'NanoTestPost':post,
+        'das':fname.replace('\n','')} 
+
+        with open('cfg/'+post[:95]+'_cfg.py', 'w') as f2:
+            f2.write(template.format(**crab_dict))
