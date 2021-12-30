ad390fa774b4c3c78380e6bad22918636ecae1f1
rymuelle <rmueller@physics.tamu.edu>
2021-12-30 22:22:31 +0100

    code to submit in a cleaner fashion

---
+++ crab_python/run_nano/crab_cfg_templates.py
@@ -0,0 +1,63 @@
+mc_template='''from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_('General')
+config.General.requestName = '{requestName}'
+config.General.transferLogs = True
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.scriptExe = 'crab_script_mc.sh'
+# hadd nano will not be needed once nano tools are in cmssw
+config.JobType.inputFiles = ['job_mc_crab.py', '../../scripts/haddnano.py', 'keep_and_drop_bff.txt']
+config.JobType.sendPythonFolder = True
+config.section_('Data')
+config.Data.inputDataset = '{das}'
+#config.Data.inputDBS = 'phys03'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 2
+
+config.Data.outLFNDirBase = '/store/group/phys_exotica/bffZprime/nanoAODskimmed/crab/{era}'
+config.Data.publication = False
+config.Data.outputDatasetTag = '{outputName}'
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+# config.section_('User')
+#config.User.voGroup = 'dcms'
+config.JobType.allowUndistributedCMSSW = True
+'''
+data_template='''from WMCore.Configuration import Configuration
+
+config = Configuration()
+
+config.section_('General')
+config.General.requestName = '{requestName}'
+config.General.transferLogs = True
+config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.scriptExe = 'crab_script_data.sh'
+# hadd nano will not be needed once nano tools are in cmssw
+config.JobType.inputFiles = ['job_data_crab.py', '../../scripts/haddnano.py', 'keep_and_drop_bff.txt', '{json}''] 
+config.JobType.sendPythonFolder = True
+config.section_('Data')
+config.Data.inputDataset = '{das}'
+#config.Data.inputDBS = 'phys03'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 1
+config.Data.lumiMask = '{json}''
+
+config.Data.outLFNDirBase = '/store/group/phys_exotica/bffZprime/nanoAODskimmed/crab/{era}'
+config.Data.publication = False
+config.Data.outputDatasetTag = '{outputName}'
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
+# config.section_('User')
+#config.User.voGroup = 'dcms'
+config.JobType.allowUndistributedCMSSW = True
+'''
---
+++ crab_python/run_nano/job_template.py
@@ -0,0 +1,179 @@
+job_data_template = '''
+#!/usr/bin/env python
+import os
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
+from PhysicsTools.NanoAODTools.postprocessing.trigger.triggerFilter import triggerFilter
+from PhysicsTools.NanoAODTools.postprocessing.bff.bffPreselModule import bffPreselProducer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.countHistogramsModule import countHistogramsProducer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.muonScaleResProducer import muonScaleRes{era} 
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles, runsAndLumis
+
+isMC = False
+dataYear = "{era}"
+#runPeriod = ""
+
+triggers= {triggers}
+
+keep_and_drop = 'keep_and_drop_bff.txt'
+
+modules=[
+    countHistogramsProducer(),
+    triggerFilter(triggers),
+    muonScaleRes{era}(),
+    bffPreselProducer(int(dataYear))
+    ]
+
+p = PostProcessor(".",
+                  inputFiles(),
+                  modules=modules,
+                  provenance=True,
+                  fwkJobReport=True,
+                  outputbranchsel=keep_and_drop,
+                  jsonInput=runsAndLumis()
+                  )
+p.run()
+
+print("DONE")
+#note: lepSF has to go before puWeight due to the lepton weight constructor redefining the weight constructor
+#-I PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer btagSF$era \
+'''
+
+job_mc_template = '''
+#!/usr/bin/env python
+import os
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
+from PhysicsTools.NanoAODTools.postprocessing.trigger.triggerFilter import triggerFilter
+from PhysicsTools.NanoAODTools.postprocessing.bff.bffPreselModule import bffPreselProducer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import createJMECorrector
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.countHistogramsModule import countHistogramsProducer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import btagSF 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.lepSFProducer import lepSF{era}
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeight_{era} 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.muonScaleResProducer import muonScaleRes{era} 
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles, runsAndLumis
+
+isMC = True
+dataYear = "{era}"
+runPeriod = ""
+
+triggers= {triggers}
+
+jmeCorrections = createJMECorrector(
+    isMC=isMC, 
+    dataYear=dataYear, 
+    runPeriod=runPeriod,
+    jesUncert="Total", 
+    applySmearing=True,
+    jetType="AK4PFchs",
+    noGroom=False)
+
+keep_and_drop = 'keep_and_drop_bff.txt'
+
+modules=[
+    countHistogramsProducer(),
+    triggerFilter(triggers),
+    btagSF(dataYear),
+    jmeCorrections(),
+    puWeight_{era}(),
+    muonScaleRes{era}(),
+    lepSF{era}(),
+    bffPreselProducer(int(dataYear), isMC=True)
+    ]
+
+p = PostProcessor(".",
+                  inputFiles(),
+                  modules=modules,
+                  provenance=True,
+                  fwkJobReport=True,
+                  outputbranchsel=keep_and_drop,
+                  )
+p.run()
+
+print("DONE")
+#note: lepSF has to go before puWeight due to the lepton weight constructor redefining the weight constructor
+#-I PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer btagSF$era \
+'''
+
+
+job_mc_local_template='''
+#!/usr/bin/env python
+import os
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
+from PhysicsTools.NanoAODTools.postprocessing.trigger.triggerFilter import triggerFilter
+from PhysicsTools.NanoAODTools.postprocessing.bff.bffPreselModule import bffPreselProducer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import createJMECorrector
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.countHistogramsModule import countHistogramsProducer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import btagSF 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.lepSFProducer import lepSF{era}
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeight_{era}
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.muonScaleResProducer import muonScaleRes{era}
+from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles, runsAndLumis
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile")
+parser.add_argument("outdir")
+parser.add_argument( '-g','--glob', default=False, action='store_true')
+parser.add_argument( '-x','--xrdcp', default=False, action='store_true')
+args = parser.parse_args()
+
+infile = args.infile
+outdir = args.outdir
+glob = args.glob
+xrdcp = args.xrdcp
+
+print(infile, outdir, glob)
+if glob:
+    import glob
+    ifilename = glob.glob(infile)
+else:
+    ifilename = [infile]
+
+if xrdcp:
+    sourceprefix="root://cms-xrd-global.cern.ch/"
+else:
+    sourceprefix="./"
+ifname = ["{{}}{{}}".format(sourceprefix, fname) for fname in ifilename]
+isMC = True
+dataYear = "{era}"
+runPeriod = ""
+
+triggers= {triggers}
+
+jmeCorrections = createJMECorrector(
+    isMC=isMC, 
+    dataYear=dataYear, 
+    runPeriod=runPeriod,
+    jesUncert="Total", 
+    applySmearing=True,
+    jetType="AK4PFchs",
+    noGroom=False)
+    
+keep_and_drop = 'keep_and_drop_bff.txt'
+
+modules=[
+    countHistogramsProducer(),
+    triggerFilter(triggers),
+    btagSF(dataYear),
+    jmeCorrections(),
+    puWeight_{era}(),
+    muonScaleRes{era}(),
+    lepSF{era}(),
+    bffPreselProducer(int(dataYear), isMC=isMC)
+    ]
+
+p = PostProcessor(outdir,
+                  ifilename,
+                  modules=modules,
+                  provenance=True,
+                  fwkJobReport=True,
+                  outputbranchsel=keep_and_drop,
+                  #maxEntries=1000,
+                  )
+p.run()
+
+print("DONE")
+#note: lepSF has to go before puWeight due to the lepton weight constructor redefining the weight constructor
+#-I PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer btagSF$era \
+'''
