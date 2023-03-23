481ac91d33b9e94e2d9a011d25ca9d6b72977e86
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2023-03-23 07:16:31 -0700

    Update local skimmer

--- crab/test_local_cfg.py
+++ crab/test_local_cfg.py
@@ -17,83 +17,144 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.common.muSFProducer2 impor
 
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.ggTemporaryScale import *
 
-#very basic selection which is covered then by the actual Hgg selection and crop at 1000 evts
-selection='''Sum$(Photon_pt > 18 && abs(Photon_eta)<2.5) > 1 && Entry$ < 1000'''
-selection='''Sum$(Photon_pt > 18 && (Photon_isScEtaEB || Photon_isScEtaEE) && Photon_electronVeto > 0.5 && Photon_hoe < 0.08) > 1 && Entry$ < 1000'''
-#work on a local file
-# a modified nanoAOD which contians extra phton features -> to be merged soon to the central stuff
-files=["/hadoop/cms/store/user/hmei/nanoaod_runII/HHggtautau/HHggtautau_Era2018_private_v2_20201005/test_nanoaod_1.root"]
+import sys
+print sys.argv
+suffix=sys.argv[2].split("=")[1]
+analysis=sys.argv[3].split("=")[1]
+
 
 selection='''Sum$(Photon_pt > 18 && (Photon_isScEtaEB || Photon_isScEtaEE) && Photon_electronVeto > 0.5 && Photon_hoe < 0.08) > 1'''
 
-f16a=["root://cmsxrootd.fnal.gov///store/mc/RunIISummer20UL16NanoAODAPVv9/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mcRun2_asymptotic_preVFP_v11-v2/2520000/09C7F740-4B72-164F-802A-AD61543C717C.root"]
 
-f16b=["root://cmsxrootd.fnal.gov///store/mc/RunIISummer20UL16NanoAODv9/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mcRun2_asymptotic_v17-v2/2520000/9503E3B6-3BC0-A043-92BF-019E4C64026D.root"]
+files=[ '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8042.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8043.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8044.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8045.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8046.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8047.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8048.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8049.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_8050.root',
+        '/ceph/cms/store/group/Hgg/XToYHToggbb/customNanoAOD/2018/EGamma_Run2018D-UL2018_MiniAODv2-v2_MINIAOD_v0/nanoaod_805.root' ]
 
-f17=["root://cmsxrootd.fnal.gov///store/mc/RunIISummer20UL17NanoAODv9/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_mc2017_realistic_v9-v2/100000/89E8691C-A390-C74F-A065-70EF7E3B8F77.root"]
-
-f18=["root://cmsxrootd.fnal.gov///store/mc/RunIISummer20UL18NanoAODv9/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/100000/5816099C-2902-3348-8627-2DEB0BBC33CD.root"]
-
-f16D=[
-  #'/ceph/cms/store/user/fsetti/Summer20UL_nanoAODv9/DoubleEG_Run2016D-HIPM_UL2016_MiniAODv2-v1_MINIAOD_fix_HIPM_v3/nanoaod_441.root',
- '/ceph/cms/store/user/fsetti/Summer20UL_nanoAODv9/DoubleEG_Run2016D-HIPM_UL2016_MiniAODv2-v1_MINIAOD_fix_HIPM_v3/nanoaod_442.root',
-#'/ceph/cms/store/user/fsetti/Summer20UL_nanoAODv9/DoubleEG_Run2016D-HIPM_UL2016_MiniAODv2-v1_MINIAOD_fix_HIPM_v3/nanoaod_443.root',
-'/ceph/cms/store/user/fsetti/Summer20UL_nanoAODv9/DoubleEG_Run2016D-HIPM_UL2016_MiniAODv2-v1_MINIAOD_fix_HIPM_v3/nanoaod_444.root'
-  
-  ]
 
 #2016 modules MC
 PrefireCorr2016 = lambda : PrefCorr("L1prefiring_jetpt_2016BtoH.root", "L1prefiring_jetpt_2016BtoH", "L1prefiring_photonpt_2016BtoH.root", "L1prefiring_photonpt_2016BtoH")
+bTagShapeSFAndUnc2016preVFP = lambda : btagSFProducer("UL2016preVFP", "deepjet", [ "shape_corr" ])
+bTagShapeSFAndUnc2016postVFP = lambda : btagSFProducer("UL2016postVFP", "deepjet", [ "shape_corr" ])
 
 #2017 modules MC
 PrefireCorr2017 = lambda : PrefCorr('L1prefiring_jetpt_2017BtoF.root', 'L1prefiring_jetpt_2017BtoF', 'L1prefiring_photonpt_2017BtoF.root', 'L1prefiring_photonpt_2017BtoF')
+bTagShapeSFAndUnc2017 = lambda : btagSFProducer("UL2017", "deepjet", [ "shape_corr" ])
 
-import sys
-suffix=sys.argv[1]
-print suffix, "SUFFIXXXX"
+#2018 modules MC
+bTagShapeSFAndUnc2018 = lambda : btagSFProducer("UL2018", "deepjet", [ "shape_corr"])
+
 
 if suffix=='16a':
-  p=PostProcessor(".",f16a,       
+  p=PostProcessor(".", 
+                  files,
                   selection.replace('\n',''),
                   branchsel="keep_and_drop.txt",
                   outputbranchsel="keep_and_drop.txt",
-                  modules=[puAutoWeight_UL2016(),jetmetUncertaintiesAPV2016UL(),jetmetUncertaintiesAPV2016ULAll(), muonScaleRes2016v5ULAPV(), PrefireCorr2016(), tauCorrs2016ULpreVFP(), lepSFID2016_B(), lepSFISO2016_B(), gammaSF_UL16PreVFP()],
-                  provenance=True)
-elif suffix=='16b' :
-   p=PostProcessor(".",f16b,     
-                  selection.replace('\n',''),
-                  branchsel="keep_and_drop.txt",
-                  outputbranchsel="keep_and_drop.txt",
-                  modules=[puAutoWeight_UL2016(),jetmetUncertainties2016UL(),jetmetUncertainties2016ULAll(), muonScaleRes2016v5UL(), PrefireCorr2016(), tauCorrs2016ULpostVFB(), lepSFID2016_H(), lepSFISO2016_H(), gammaSF_UL16PostVFP()],
-                  provenance=True)
-elif suffix=='17':
-   p=PostProcessor(".",f17, 
-                  selection.replace('\n',''),
-                  branchsel="keep_and_drop.txt",
-                  outputbranchsel="keep_and_drop.txt",
-                  modules=[puAutoWeight_UL2017(),jetmetUncertainties2017UL(),jetmetUncertainties2017ULAll(), muonScaleRes2017v5UL(), PrefireCorr2017(), tauCorrs2017UL(), lepSFID2017(), lepSFISO2017(), gammaSF_UL17()],
-                  provenance=True)
-elif suffix=='18':
-   p=PostProcessor(".",f18, 
-                  selection.replace('\n',''),
-                  branchsel="keep_and_drop.txt",
-                  outputbranchsel="keep_and_drop.txt",
-                  modules=[puAutoWeight_UL2018(),jetmetUncertainties2018UL(),jetmetUncertainties2018ULAll(), muonScaleRes2018v5UL(), tauCorrs2018UL(), lepSFID2018(), lepSFISO2018(), gammaSF_UL18()],
-                  provenance=True)
-elif suffix=='16D':
-   p=PostProcessor(".",f16D, 
-                  selection.replace('\n',''),
-                  branchsel="keep_and_drop.txt",
-                  outputbranchsel="keep_and_drop.txt",
-                  modules=[muonScaleRes2016v5ULAPV()],
-                  provenance=True)
+                  modules=([puAutoWeight_UL2016(), jetmetUncertaintiesAPV2016UL(), jetmetUncertaintiesAPV2016ULAll(), muonScaleRes2016v5ULAPV(), PrefireCorr2016(), tauCorrs2016ULpreVFP(), lepSFID2016_B(), lepSFISO2016_B(), gammaSF_UL16PreVFP()] if analysis=="ggtautau" \
+                      else [puAutoWeight_UL2016(), jetmetUncertaintiesAPV2016UL(), jetmetUncertaintiesAPV2016ULAll(), bTagShapeSFAndUnc2016preVFP(), gammaSF_UL16PreVFP()] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
 
+elif suffix=='16b' :
+  p=PostProcessor(".", 
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([puAutoWeight_UL2016(), jetmetUncertainties2016UL(), jetmetUncertainties2016ULAll(), muonScaleRes2016v5UL(), PrefireCorr2016(), tauCorrs2016ULpostVFB(), lepSFID2016_H(), lepSFISO2016_H(), gammaSF_UL16PostVFP()] if analysis=="ggtautau" \
+                      else [puAutoWeight_UL2016(), jetmetUncertainties2016UL(), jetmetUncertainties2016ULAll(), bTagShapeSFAndUnc2016postVFP(), gammaSF_UL16PostVFP()] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+elif suffix=='17':
+  p=PostProcessor(".", 
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([puAutoWeight_UL2017(), jetmetUncertainties2017UL(), jetmetUncertainties2017ULAll(), muonScaleRes2017v5UL(), PrefireCorr2017(), tauCorrs2017UL(), lepSFID2017(), lepSFISO2017(), gammaSF_UL17()] if analysis=="ggtautau" \
+                      else [puAutoWeight_UL2017(), jetmetUncertainties2017UL(), jetmetUncertainties2017ULAll(), bTagShapeSFAndUnc2017(), gammaSF_UL17()] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+elif suffix=='18':
+  p=PostProcessor(".", 
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([puAutoWeight_UL2018(), jetmetUncertainties2018UL(), jetmetUncertainties2018ULAll(), muonScaleRes2018v5UL(), tauCorrs2018UL(), lepSFID2018(), lepSFISO2018(), gammaSF_UL18()] if analysis=="ggtautau" \
+                      else [puAutoWeight_UL2018(), jetmetUncertainties2018UL(), jetmetUncertainties2018ULAll(), bTagShapeSFAndUnc2018(), gammaSF_UL18()] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+elif suffix=='16aD':
+  p=PostProcessor(".",
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([muonScaleRes2016v5ULAPV()] if analysis=="ggtautau" \
+                      else [] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+elif suffix=='16bD' :
+  p=PostProcessor(".",
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([muonScaleRes2016v5UL()] if analysis=="ggtautau" \
+                      else [] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+elif suffix=='17D':
+  p=PostProcessor(".",
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([muonScaleRes2017v5UL()] if analysis=="ggtautau"\
+                      else [] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+elif suffix=='18D':
+  p=PostProcessor(".",
+                  files,
+                  selection.replace('\n',''),
+                  branchsel="keep_and_drop.txt",
+                  outputbranchsel="keep_and_drop.txt",
+                  modules=([muonScaleRes2018v5UL()] if analysis=="ggtautau" \
+                      else [] if analysis=="ggbb" \
+                      else []),
+                  provenance=True
+                  )
+
+else:
+  print "bo"
 
 
 # keep and drop printout
 print p.branchsel._ops
 print p.outputbranchsel._ops
 
-#run locally
 p.run()
 
+print("DONE")
