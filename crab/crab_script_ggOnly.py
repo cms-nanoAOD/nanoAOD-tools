94f6476536b98e667828e9846c006bba0243aa6d
Ian Reed <ireed3597@protonmail.com>
2023-07-07 08:55:57 -0700

    Changes for the DY ABCD data driven estimation

--- crab/crab_script_ggOnly.py
+++ crab/crab_script_ggOnly.py
@@ -23,7 +23,7 @@ suffix=sys.argv[2].split("=")[1]
 analysis=sys.argv[3].split("=")[1]
 
 
-selection='''Sum$(Photon_pt > 18 && (Photon_isScEtaEB || Photon_isScEtaEE) && Photon_electronVeto > 0.5 && Photon_hoe < 0.08) > 1'''
+selection=''''''
 
 
 #2016 modules MC
