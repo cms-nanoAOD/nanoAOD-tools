456b482f70dd09be68c34b8065ca906bd1424124
IzaakWN <iwn_@hotmail.com>
2023-08-08 17:34:55 +0200

    em dash -> normal dash

--- python/postprocessing/examples/exampleGenDump.py
+++ python/postprocessing/examples/exampleGenDump.py
@@ -8,6 +8,8 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import Pos
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 #from PhysicsTools.NanoAODTools.postprocessing.tools import hasbit
+import PhysicsTools.NanoAODTools.postprocessing.framework.datamodel as datamodel
+datamodel.statusflags['isHardPrompt'] = datamodel.statusflags['isPrompt'] + datamodel.statusflags['fromHardProcess'] 
 
 
 def getprodchain(part,genparts=None,event=None,decay=-1):
@@ -17,10 +19,10 @@ def getprodchain(part,genparts=None,event=None,decay=-1):
   while imoth>=0:
     if genparts:
       moth = genparts[imoth]
-      chain = "%3s –> "%(moth.pdgId)+chain
+      chain = "%3s -> "%(moth.pdgId)+chain
       imoth = moth.genPartIdxMother
     elif event:
-      chain = "%3s –> "%(event.GenPart_pdgId[imoth])+chain
+      chain = "%3s -> "%(event.GenPart_pdgId[imoth])+chain
       imoth = event.GenPart_genPartIdxMother[imoth]
   if genparts and decay>0:
     chain = chain[:-3] # remove last particle
@@ -40,9 +42,9 @@ def getdecaychain(part,genparts,indent=0,depth=999):
       if ndaus>=1:
         chain += '\n'+' '*indent_
       if depth>=2:
-        chain += " –> "+getdecaychain(dau,genparts,indent=indent_+4,depth=depth-1)
+        chain += " -> "+getdecaychain(dau,genparts,indent=indent_+4,depth=depth-1)
       else: # stop recursion
-        chain += " –> %3s"%(dau.pdgId)
+        chain += " -> %3s"%(dau.pdgId)
       ndaus += 1
   return chain
   
@@ -57,7 +59,7 @@ class LHEDumper(Module):
   
   def analyze(self,event):
     """Dump gen information for each gen particle in given event."""
-    print("\n%s Event %s %s"%('–'*10,event.event,'–'*70))
+    print("\n%s Event %s %s"%('-'*10,event.event,'-'*70))
     self.nevents += 1
     leptonic = False
     tauonic = False
@@ -73,6 +75,7 @@ class LHEDumper(Module):
       prompt   = particle.statusflag('isPrompt') #hasbit(particle.statusFlags,0)
       taudecay = particle.statusflag('isTauDecayProduct') #hasbit(particle.statusFlags,2)
       lastcopy = particle.statusflag('isLastCopy') #hasbit(particle.statusFlags,13)
+      ishardprompt = particle.statusflag('isHardPrompt')
       if 0<=mothidx<len(particles):
         moth    = particles[mothidx]
         mothpid = moth.pdgId
@@ -102,11 +105,11 @@ class LHEDumper(Module):
       self.ntauonic += 1
   
   def endJob(self):
-    print('\n'+'–'*96)
+    print('\n'+'-'*96)
     if self.nevents>0:
       print("  %-10s %4d / %-4d (%4.1f%%)"%('Tauonic: ',self.ntauonic, self.nevents,100.0*self.ntauonic/self.nevents))
       print("  %-10s %4d / %-4d (%4.1f%%)"%('Leptonic:',self.nleptonic,self.nevents,100.0*self.nleptonic/self.nevents))
-    print("%s Done %s\n"%('–'*10,'–'*80))
+    print("%s Done %s\n"%('-'*10,'-'*80))
   
 
 # PROCESS NANOAOD
